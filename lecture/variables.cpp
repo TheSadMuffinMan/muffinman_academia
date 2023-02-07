c6fceceda421c1e98dd12fddb28e7875f53c6328
TheSadMuffinMan <albusgithub@mevacy.com>
2023-02-07 09:37:59 -0700

    Backslash n changes

--- lecture/variables.cpp
+++ lecture/variables.cpp
@@ -21,7 +21,7 @@ int main() {
     // cin >> string_last;
 
     // string_full_name = string_first + " " + string_last;
-    // // num1 + num2 = total IS THE WRONG WAY
+    // num1 + num2 = total IS THE WRONG WAY
 
     // cout << "Welcome " << string_full_name << endl;
 
@@ -29,12 +29,12 @@ int main() {
 
     // number1 = 17;
 
-    // cout << "The memory address of number1 is: " << &number1 << endl;
-    // cout << "The value of number1 is: " << number1 << endl;
+    // cout << "The memory address of number1 is: " << &number1 << \n;
+    // cout << "The value of number1 is: " << number1 << \n;
 
     // number1 = 76;
 
-    // cout << "The memory address of number1 now is: " << &number1 << endl;
+    // cout << "The memory address of number1 now is: " << &number1 << \n;
     // cout << "The value of number1 is now: " << number1 << endl;
 
 
