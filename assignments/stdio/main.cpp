d9be6d07beb7e5327bb8d45498b31781b6863aa8
TheSadMuffinMan <albusgithub@mevacy.com>
2023-02-09 13:34:50 -0700

    setup and ez stuff

---
+++ assignments/stdio/main.cpp
@@ -0,0 +1,27 @@
+/* Howdy!
+
+Name: Anthony Streich
+9 Feb 2023
+
+This program is meant to replicated the various stages of the classic game, hangman.
+
+Uh.. Other programming info?
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Greetings gamer, please enter your full name: ";
+    getline(cin, full_name);
+
+    cout << "Welcome " << full_name << "." << endl;
+    cout << "The hangman game is under construction, maybe you\'ll get to play it in a few weeks…" << endl;
+    cout << "This is what various stages of the hangman game will look like… \n \n";
+
+
+
+
+    return 0;
+}
