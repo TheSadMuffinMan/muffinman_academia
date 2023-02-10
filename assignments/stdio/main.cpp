666f98ac5451df10faab286aefe83ac8ae5dfdef
TheSadMuffinMan <albusgithub@mevacy.com>
2023-02-10 09:44:29 -0700

    Working cout gallows

--- assignments/stdio/main.cpp
+++ assignments/stdio/main.cpp
@@ -14,24 +14,25 @@ using namespace std;
 
 int main() {
     cout << "Greetings gamer, please enter your full name: ";
+    string full_name;
     getline(cin, full_name);
 
-    cout << "Welcome " << full_name << "." << endl;
+    cout << "Welcome " << full_name << ".\n" << endl;
     cout << "The hangman game is under construction, maybe you\'ll get to play it in a few weeks…" << endl;
     cout << "This is what various stages of the hangman game will look like… \n \n";
 
 
-    string line_1 = "|----------------- ";
-    string line_2 = "|/        	|  ";
-    string line_3 = "|";
-    string line_4 = "|";
-    string line_5 = "|";
-    string line_6 = "|";
-    string line_7 = "|";
-    string line_8 = "|";
-    string line_9 = " ===========";
+    string line_1 = " |----------------- \n";
+    string line_2 = " |/        	|  \n";
+    string line_3 = " |\n";
+    string line_4 = " |\n";
+    string line_5 = " |\n";
+    string line_6 = " |\n";
+    string line_7 = " |\n";
+    string line_8 = " |\n";
+    string line_9 = "  ===========\n";
  
-
+    cout << line_1 << line_2 << line_3 << line_4 << line_5 << line_6 << line_7 << line_8 << line_9;
 
 
     return 0;
