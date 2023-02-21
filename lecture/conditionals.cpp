9fd3d3f58d38bceaab9121cb8f5edd3da5f136bd
TheSadMuffinMan <albusgithub@mevacy.com>
2023-02-21 09:31:33 -0700

    Skeleton

---
+++ lecture/conditionals.cpp
@@ -0,0 +1,20 @@
+/*
+Name: Anthony Streich
+Functions pass-by-reference/pass-by-value
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int num1, num2;
+
+    num1 = 10;
+    num2 = 20;
+
+    cout << (num1 == num2); << endl;
+
+    return 0;
+}
