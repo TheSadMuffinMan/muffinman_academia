d50157d36694e994182b8a67d470d13b59ac0c7a
TheSadMuffinMan <albusgithub@mevacy.com>
2023-03-01 09:12:40 -0700

    Classwork

--- 1_lecture/7_loops.cpp
+++ 1_lecture/7_loops.cpp
@@ -17,15 +17,32 @@ int main() {
     userInput(userName);
     greetUser(userName);
 
+// First exposure to for loops
+/*
+    for (int something = 0; something < 10; something++) {
+        cout << "i: " << something << endl;
+    }
+    */
+
+// The i++ is signifying that each number will go up by one
+// Can be shown as... Also works with "-"
+// i++;
+// i = 1 + 1;
+// i += 1;
+// ++i;
+   for (int i = 0; i < 10; i++) {
+        cout << "i: " << i << endl;
+    }
+
     return 0;
 }
 
 void userInput(string &userName) {
-    cout << "Please enter your name, and then press enter.\n" << endl;
+    cout << "\nPlease enter your name, and then press enter." << endl;
     getline(cin, userName);
 }
 
 void greetUser(string userName) {
-    cout << "Welcome " << userName << " to my program!" << endl;
+    cout << "\nWelcome " << userName << " to my program!\n" << endl;
 }
     
