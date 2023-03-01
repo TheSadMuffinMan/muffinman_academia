331c3f489196e3651438388461b32a4010cc9e3f
TheSadMuffinMan <albusgithub@mevacy.com>
2023-03-01 09:50:21 -0700

    All classwork 1 March

--- 1_lecture/7_loops.cpp
+++ 1_lecture/7_loops.cpp
@@ -17,15 +17,17 @@ int main() {
     userInput(userName);
     greetUser(userName);
 
-    int j = 0;
-
-// First exposure to for loops
 /*
+// First exposure to for loops
+// The first part is called the initialization/declaration
+// The middle part is called the conditional
+// The last part is called the incriment/updatation
     for (int something = 0; something < 10; something++) {
         cout << "i: " << something << endl;
     }
     */
 
+/*
 // The i++ is signifying that each number will go up by one
 // The i's are temporary variables, and the initial part (int i = 0) is intializing the variable
 // Typically use i, j, k, m, or n as temp variables in the for loops
@@ -34,12 +36,22 @@ int main() {
 // i = 1 + 1;
 // i += 1;
 // ++i;
-
     for (int i = 0; i > 0; i--) {
         j = j + 1;
         cout << i << ": Hello world" << endl;
         cout << "j: " << j << endl;
     }
+*/
+
+/*
+// This will create an infinite loop
+// It has no initialization/declaration (the beginning part)
+// It has no conditional (the middle part)
+// It has no incriment (the last part)/updatation
+    for ( ; ; ) {
+        cout << i << ": Hello world" << endl;
+    }
+*/
 
 /* This prints out..
 0: Hello world!
@@ -49,7 +61,21 @@ int main() {
    for (int i = 0; i < 10; i++) {
         cout << i << ": Hello world!" << endl;
     }
-    */
+*/
+
+/*
+    for (int i = 0, j = 0; i < 10 || j > 0; i++, j--) {
+        cout << i << ": Hello world!" << endl;
+        cout << j << ": Goodbye world!" << endl;
+    }
+*/
+
+    // This is called nesting a for loop (including a loop inside a loop)
+    for (int i = 0; i < 10; i++) {
+        for (int j = 9; j > 0; j--) {
+            cout << "i: " << i << " j: " << j << endl;
+        }
+    }
 
     return 0;
 }
