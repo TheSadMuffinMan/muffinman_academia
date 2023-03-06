bccd999c3bc0ada550e6d231b4323ae10ebe3a32
TheSadMuffinMan <albusgithub@mevacy.com>
2023-03-06 09:50:46 -0700

    Classwork 6 Feb 23

--- 1_lecture/9_multiplication_table.cpp
+++ 1_lecture/9_multiplication_table.cpp
@@ -7,29 +7,49 @@ Making a multiplication table
 
 #include <iostream>
 #include <string>
+
+// Used for printf
 #include <cstdio>
+
+// Used for setw()
 #include <iomanip>
 
 using namespace std;
 
 void userInput(string&);
 void greetUser(string userName);
+void printTable(height, width);
+void promptSize(int, int);
 
 int main(int argc, char *argv[]) {
     string userName;
+    int height, width;
+
     userInput(userName);
     greetUser(userName);
+    printTable(height, width);
 
-    // Starting at 0, up to ten, updating the function by 1
-    for(int i=1; i<=10; i++) {
+    promptSize(height, width);
+
+    return 0;
+}
+
+void promptSize(int& height, int& width) {
+    cout << "How tall do you want your table to be? ";
+    cin >> height;
+
+    cout << "How wide do you want your table? ";
+    cin >> width;
+}
+
+void printTable(int height, int width) {
+    for(int i=1; i<= height; i++) {
         // Can also use (int j=1; j<11; j++), they're functionally the same
-        for(int j=1; j<=10; j++) {
+        for(int j=1; j<= width; j++) {
             cout << setw(5) << (j*i);
         }
         cout << endl;
     }
-
-    return 0;
 }
 
 void userInput(string& userName) {
