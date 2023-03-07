d0e18f6f3ee53618ce10902331c3b243aa42c1f3
TheSadMuffinMan <albusgithub@mevacy.com>
2023-03-07 09:20:00 -0700

    Mid class work

--- 1_lecture/10_pointers.cpp
+++ 1_lecture/10_pointers.cpp
@@ -16,6 +16,17 @@ int main(int argc, char *argv[]) {
     string userName;
     userInput(userName);
     greetUser(userName);
+    // If you are creating a pointer, the pointer type has to match what it is pointing to
+    // For example, if you are pointing to an int, your pointer must be int *ptr1
+    int num1;
+    int *ptr1;
+
+    num1 = 42;
+    ptr1 = &num1;
+
+    cout << "&num1: " << &num1 << endl;
+    cout << "ptr1: " << ptr1 << endl;
+    cout << "&ptr1: " << &ptr1 << endl;
 
     return 0;
 }
