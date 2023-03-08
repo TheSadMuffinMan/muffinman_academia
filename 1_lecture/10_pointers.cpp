9a2cc40dbf21221fd8b5f183fe7409fae8067e37
TheSadMuffinMan <albusgithub@mevacy.com>
2023-03-08 09:48:58 -0700

    Class work 8 march 23

--- 1_lecture/10_pointers.cpp
+++ 1_lecture/10_pointers.cpp
@@ -12,29 +12,55 @@ using namespace std;
 void userInput(string&);
 void greetUser(string userName);
 
+int addNums(int*, int*);
+
 int main(int argc, char *argv[]) {
     // string userName;
     // userInput(userName);
     // greetUser(userName);
 
-    int num1 = 0;
+    int n1, n2;
+    int *ptr1, *ptr2;
+
+    n1 = 42;
+    n2 = 15;
+    ptr1 = &n1;
+    ptr2 = &n2;
+
+    cout << n1 << " + " << n2 << " = " << addNums(&n1, &n2) << endl;
+
+    // This allows you to make conditionals with pointers
+    // int *somePointer1 = NULL;
+
+    /*
+    // If you're looking for the actual value, you can also have *(&num1)
+    // If you look inside the pointer, you will only see random numbers
+    cout << "&num1: " << &num1 << endl;
+    // &somePointer1 will output the memory address of the pointer itself
+    // *somePointer1 will output the value of the dereferenced variable
+    cout << "somePointer1: " << *somePointer1 << endl;
+    */
+
+    /*
+    // This is where the NULL portion is applicabale
+    if (somePointer1 == NULL) {
+       somePointer1 = &num1;
+    } */
+
+    // int num1 = 0;
     // float fnum1;
     // string snum1;
     // char cnum1;
 
     // This is the C++ way of doing things for null pointers
-    int *ptr1 = nullptr;
+    // int *ptr1 = nullptr;
     // This is the old C way of doing things, case sensitive
-    ptr1 = NULL;
+    // ptr1 = NULL;
     // This will also work
-    ptr1 = __null;
+    // ptr1 = __null;
 
-    if (ptr1 == NULL) {
-        ptr1 = &num1;
-    }
-
-    cout << "&num1: " << &num1 << endl;
-    cout << "ptr1: " << ptr1 << endl;
+    // cout << "&num1: " << &num1 << endl;
+    // cout << "ptr1: " << ptr1 << endl;
 
     /*
     // If you are creating a pointer, the pointer type has to match what it is pointing to
@@ -54,15 +80,11 @@ int main(int argc, char *argv[]) {
     *ptr1 = 15;
     */
 
-    // cout << "&num1: " << &num1 << endl;
-    // cout << "&num2: " << &num2 << endl;
-    // cout << "&num3: " << &num3 << endl;
-    // cout << "ptr1: " << ptr1 << endl;
-    // cout << "&ptr1: " << &ptr1 << endl;
-    // cout << "&ptr2: " << &ptr2 << endl;
-    // cout << "&ptr3: " << &ptr3 << endl;
-
     return 0;
+}
+
+int addNums(int *num1, int *num2) {
+    return *num1 + *num2;
 }
 
 void userInput(string& userName) {
