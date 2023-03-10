07dbd953af050e71bf5e20ef61e450973fd8be83
TheSadMuffinMan <albusgithub@mevacy.com>
2023-03-10 11:42:19 -0700

    Good progress, working with grease now

--- 2_assignments/functions_homework3/functions.cpp
+++ 2_assignments/functions_homework3/functions.cpp
@@ -14,25 +14,33 @@ void userInput(string&);
 void greetUser(string userName);
 
 void addNums(float, float);
+void multNums(float, float);
 
 int main(int argc, char *argv[]) {
-    string userName;
-    userInput(userName);
-    greetUser(userName);
+    // string userName;
+    // userInput(userName);
+    // greetUser(userName);
 
-    float userNum1 = 0;
-    float userNum2 = 0;
+    float userNum1, userNum2;
+    cout << "Please enter two numbers seperated by a space: " << endl;
+    cin >> userNum1 >> userNum2;
 
     addNums(userNum1, userNum2);
+    multNums(userNum1, userNum2);
 
     return 0;
 }
 
 void addNums(float userNum1, float userNum2) {
-    cout << "Please enter two numbers that you want to add together seperated by a space: " << endl;
-    cin >> userNum1 >> userNum2;
+    // cout << "Please enter two numbers that you want to add together seperated by a space: " << endl;
+    // cin >> userNum1 >> userNum2;
     float addition = userNum1 + userNum2;
     cout << userNum1 << " + " << userNum2 << " = " << addition << endl;
+}
+
+void multNums(float userNum1, float userNum2) {
+    float multiplication = userNum2 * userNum1;
+    cout << userNum1 << " x " << userNum2 << " = " << multiplication << endl;
 }
 
 void userInput(string& userName) {
