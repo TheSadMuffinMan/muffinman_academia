c4f67a95739705d3251fc0c8f726127e8f0e2de5
TheSadMuffinMan <albusgithub@mevacy.com>
2023-03-27 14:11:39 -0600

    Progress

--- 2_homework/4_conditional/conditional.cpp
+++ 2_homework/4_conditional/conditional.cpp
@@ -14,7 +14,7 @@ using namespace std;
 void userInput(string&);
 void greetUser(string userName);
 
-int promptNums(int, int, int, int, int);
+void getNums(int, int, int, int, int);
 int totalSum(int, int, int, int, int);
 
 int main(int argc, char *argv[]) {
@@ -22,12 +22,8 @@ int main(int argc, char *argv[]) {
     userInput(userName);
     greetUser(userName);
 
-    int num1, num2, num3, num4, num5;
-
-    promptNums(num1, num2, num3, num4, num5);
-
-    cout << "Press enter to quit: \n";
-    cin.get();
+    int n1, n2, n3, n4, n5;
+    getNums(n1, n2, n3, n4, n5);
 
     return 0;
 }
@@ -39,15 +35,15 @@ void userInput(string& userName) {
 
 void greetUser(string userName) {
     // Choose between the two
-    // printf("Welcome %s to our multiplication table printer!\n", userName.c_str());
+    // printf("Welcome %s to my program!\n", userName.c_str());
     cout << "Welcome " << userName << " to my program!\n" << endl;
 }
 
-int promptNums(int num1, int num2, int num3, int num4, int num5){
+void getNums(int n1, int n2, int n3, int n4, int n5){
     cout << "Please enter 5 numbers, seperated by hitting enter: " << endl;
-    cin >> num1 >> num2 >> num3 >> num4 >> num5;
+    cin >> n1 >> n2 >> n3 >> n4 >> n5;
 }
 
-int totalSum(int &num1, int &num2, int &num3, int &num4, int &num5){
+int totalSum(int num1, int num2, int num3, int num4, int num5){
     return num1 + num2 + num3 + num4 + num5;
 }
