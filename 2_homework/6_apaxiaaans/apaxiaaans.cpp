696f2d3db042a738b580fc858e4c94a969c1613f
TheSadMuffinMan <albusgithub@mevacy.com>
2023-04-19 10:42:21 -0600

    Very beginning

---
+++ 2_homework/6_apaxiaaans/apaxiaaans.cpp
@@ -0,0 +1,16 @@
+/*
+Name: Anthony Streich
+Date: 19 March 23
+Kattis Apaxiaaans!
+CURRENT ISSUES: 
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    
+    return 0;
+}
