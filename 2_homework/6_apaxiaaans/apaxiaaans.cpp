c2079143a2fbc0c9ae9b2ce5eece9757a9467682
TheSadMuffinMan <albusgithub@mevacy.com>
2023-04-20 09:50:32 -0600

    Classwork

--- 2_homework/6_apaxiaaans/apaxiaaans.cpp
+++ 2_homework/6_apaxiaaans/apaxiaaans.cpp
@@ -1,6 +1,6 @@
 /*
 Name: Anthony Streich
-Date: 19 March 23
+Date: 20 March 23
 Kattis Apaxiaaans!
 CURRENT ISSUES: 
 */
@@ -11,11 +11,18 @@ CURRENT ISSUES:
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    char activeComparisonChar, comparedChar;
+    // char activeComparisonChar, comparedChar;
     string userString;
     cout << "Program start, welcome to my Apaxiaaans Program.\n";
     cout << "Please enter the string you'd like shortened: ";
-    getline(cin.userString);
+    getline(cin,userString);
+
     cout << "**DEBUG** userString: " << userString << endl;
+
+    for (int i = 0; i < userString.size(), i++;) {
+        cout << "userString[" << i << "]: " << userString << endl;
+    }
+    
+    
     return 0;
 }
