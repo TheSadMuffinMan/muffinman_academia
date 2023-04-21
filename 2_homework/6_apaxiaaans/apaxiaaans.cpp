12d4471f85787edcd6b03ba625720cf0c109ff47
TheSadMuffinMan <albusgithub@mevacy.com>
2023-04-21 09:46:38 -0600

    Working!!

--- 2_homework/6_apaxiaaans/apaxiaaans.cpp
+++ 2_homework/6_apaxiaaans/apaxiaaans.cpp
@@ -11,18 +11,23 @@ CURRENT ISSUES:
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    // char activeComparisonChar, comparedChar;
+    char activeComparisonChar;
     string userString;
     cout << "Program start, welcome to my Apaxiaaans Program.\n";
     cout << "Please enter the string you'd like shortened: ";
     getline(cin,userString);
 
-    cout << "**DEBUG** userString: " << userString << endl;
+    // cout << "**DEBUG** userString: " << userString << endl;
 
-    for (int i = 0; i < userString.size(), i++;) {
-        cout << "userString[" << i << "]: " << userString << endl;
+    for (int i = 0; i < userString.size(); i++) {
+        // cout << "**DEBUG** userString[" << i << "]: " << userString << endl;
+        activeComparisonChar = userString[i];
+        if (activeComparisonChar == userString[i+1]) {
+            userString.erase(i,1);
+            i--;
+            cout << "**DEBUG**" << userString << endl;
+        }
     }
-    
     
     return 0;
 }
