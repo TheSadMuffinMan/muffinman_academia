5f8a2aa7092cbd9a2bdd5abfcb9fbd03cb4cd8a0
TheSadMuffinMan <albusgithub@mevacy.com>
2023-05-02 14:55:14 -0600

    Working

--- 5_extra/eng_112_companion/companion_russia.cpp
+++ 5_extra/eng_112_companion/companion_russia.cpp
@@ -10,21 +10,27 @@ CURRENT ISSUES:
 
 using namespace std;
 
+using money_int = long long int; /* This creates a new type of variable that can hold really large values. This is needed because
+a regular variable type of int cannot hold data that large, and the oligarchs have A LOT of money. */
+
 // #define clear() system("clear") // This line defines the clear function so that it can be used in the terminal.
 
 struct Character {
     string name;
     int health;
-    int money;
+    money_int money;
     int age;
     string occupation;
     string familyMembers;
 };
 
 void userInput(string&);
-void gameInfo(string userName);
+void gameInfo(string);
 void populateInfo(Character&);
+int populateGood();
+int populateBad();
 void printToons(Character);
+// void game();
 
 int main(int argc, char *argv[]) {
     string userName;
@@ -35,7 +41,12 @@ int main(int argc, char *argv[]) {
     gameInfo(userName); // Prints general game info
 
     populateInfo(oligarch); // Function that creates all the characters
+    populateGood(); // Function that populates the "good" array
+    populateBad(); // Function that populates the "bad" array
     printToons(oligarch); // Populates the toon info with struct information
+
+    //game(); // Where the actual story is played
+    //results(); // Prints out player results.
 
     return 0;
 }
@@ -50,16 +61,45 @@ void gameInfo(string userName) {
     cout << "This program is designed to tell a story that follows the lives of different people currently living in Russia." << endl;
     cout << "Hopefully these stories are able to bring better perspective to the ongoing conditions in Russia." << endl;
     cout << "The only input you will need is the numpad for character choice selections." << endl;
+    cout << "It is important to note that this game is NOT fair, and every decision will have consequences." << endl;
     cout << " .\n .\n .\n .\n \nGood luck, and stay alive.\n\n" << endl;
 }
 
 void populateInfo(Character &oligarch) {
     oligarch.name = "Bob";
     oligarch.health = 100;
+    oligarch.money = 500000000;
     oligarch.age = 50;
     oligarch.occupation = "Oil Tycoon";
     oligarch.familyMembers = "Wife, two sons, one daughter";
 }
+
+int populateGood() {
+    string goodActions[10];
+    goodActions[0] = "Money Increase";
+    goodActions[1] = "Help Someone Else";
+    goodActions[2] = "Quality time with Family";
+    goodActions[3] = "STORY DEBUG1";
+    goodActions[4] = "STORY DEBUG2";
+    return 0;
+}
+
+int populateBad() {
+    string badActions[10];
+    badActions[0] = "Draft Notice";
+    badActions[1] = "State Abduction";
+    badActions[2] = "Health Failing";
+    badActions[3] = "Mugging";
+    badActions[4] = "Inclement Weather";
+    badActions[5] = "Job Loss";
+    badActions[6] = "Food Loss";
+    badActions[7] = "Loss of Family";
+    badActions[8] = "Severe Injury";
+    badActions[9] = "Fight";
+    badActions[10] = "Transportation Loss";
+    return 0;
+}
+
 
 void printToons(Character oligarch) {
     cout << "\t\tCHARACTER ONE, RUSSIAN OLIGARCH" << endl;
@@ -81,6 +121,4 @@ void printToons(Character oligarch) {
     // cout << "\tAge: " << endl;
     // cout << "\tOccupation: " << endl;
     // cout << "\tFamily Members: " << endl;
-
-
 }
