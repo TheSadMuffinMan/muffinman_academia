2e9a1954c9cf8e6aca28e6d711079bbac24a2470
TheSadMuffinMan <albusgithub@mevacy.com>
2023-05-02 09:50:19 -0600

    Companion work, framework stuff

--- 5_extra/eng_112_companion/companion_russia.cpp
+++ 5_extra/eng_112_companion/companion_russia.cpp
@@ -10,15 +10,19 @@ CURRENT ISSUES:
 
 using namespace std;
 
+// #define clear() system("clear") // This line defines the clear function so that it can be used in the terminal.
+
 void userInput(string&);
 void gameInfo(string userName);
 void toonInfo();
 
 int main(int argc, char *argv[]) {
     string userName;
-    userInput(userName);
-    gameInfo(userName);
-    toonInfo();
+
+    // clear(); // Clears the screen
+    userInput(userName); // Takes user name
+    gameInfo(userName); // Prints general game info
+    toonInfo(); // Prints out playable character info
 
     return 0;
 }
@@ -29,11 +33,11 @@ void userInput(string& userName) {
 }
 
 void gameInfo(string userName) {
-    cout << "\nWelcome " << userName << " to my program!\n" << endl;
+    cout << "\nWelcome " << userName << " to my companion piece." << endl;
     cout << "This program is designed to tell a story that follows the lives of different people currently living in Russia." << endl;
     cout << "Hopefully these stories are able to bring better perspective to the ongoing conditions in Russia." << endl;
-    cout << "The only input you will need in the numpad for character choice selections." << endl;
-    cout << ".\n .\n .\n .\n Good luck, and stay alive.\n\n" << endl;
+    cout << "The only input you will need is the numpad for character choice selections." << endl;
+    cout << " .\n .\n .\n .\n \nGood luck, and stay alive.\n\n" << endl;
 }
 
 void toonInfo() {
@@ -41,5 +45,19 @@ void toonInfo() {
     cout << "\tName: " << endl;
     cout << "\tAge: " << endl;
     cout << "\tOccupation: " << endl;
+    cout << "\tFamily Members: \n" << endl; // This line is the first seperating line
+    
+    cout << "\t\tCHARACTER TWO" << endl;
+    cout << "\tName: " << endl;
+    cout << "\tAge: " << endl;
+    cout << "\tOccupation: " << endl;
+    cout << "\tFamily Members: " << endl; // This line is the second seperating line
+
+    cout << "\t\tCHARACTER THREE" << endl;
+    cout << "\tName: " << endl;
+    cout << "\tAge: " << endl;
+    cout << "\tOccupation: " << endl;
     cout << "\tFamily Members: " << endl;
+
+
 }
