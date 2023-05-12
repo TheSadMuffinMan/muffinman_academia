45e37f9ba5ce1d2417add381af2bae4af5d3601d
TheSadMuffinMan <albusgithub@mevacy.com>
2023-05-12 09:50:32 -0600

    Array work

---
+++ 2_homework/8_finalproject/tictactoe_game.cpp
@@ -0,0 +1,36 @@
+/*
+Name: Anthony Streich
+Date: 12 May 23
+NAME/FUNCTION OF PROGRAM
+CURRENT ISSUES: 
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// struct box {
+//     int stuff;
+// };
+
+void printBoard();
+
+int main(int argc, char *argv[]) {
+    int board[8];
+    board[0] = 0;
+    board[1] = 0;
+    board[2] = 0;
+    board[3] = 0;
+    board[4] = 0;
+    board[5] = 0;
+    board[6] = 0;
+    board[7] = 0;
+    board[8] = 0;
+
+    return 0;
+}
+
+void printBoard() {
+
+}
