d2b54d93e657f24d431be32912db869422c5fe95
TheSadMuffinMan <albusgithub@mevacy.com>
2023-05-17 10:17:08 -0600

    Solving for Carrots complete

---
+++ 4_kattis/4_solvingForCarrots.cpp
@@ -0,0 +1,18 @@
+/*
+Prints "Hello World!"
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int n, p;
+    string input1, input2;
+
+    cin >> n >> p >> input1 >> input2;
+
+    cout << p;
+    
+    return 0;
+}
