4a59d0637b26e119139f4b0df46787824ce3d2aa
TheSadMuffinMan <albusgithub@mevacy.com>
2023-05-17 11:01:11 -0600

    Work done on it

---
+++ 4_kattis/7_quadrantSelection.cpp
@@ -0,0 +1,24 @@
+/*
+Determines what quadrant an x,y point is in
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int x, y;
+    cin >> x >> y;
+
+    if ((x < 0) && (y < 0)) {
+        cout << "1" << endl;
+    } else if ((x < 0) && (y > 0)) {
+        cout << "2" << endl;
+    } else if ((x > 0) && (y > 0)) {
+        cout << "3" << endl;
+    } else {
+        cout << "4" << endl;
+    }
+    
+    return 0;
+}
