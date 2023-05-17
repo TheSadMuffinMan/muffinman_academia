7f54c6da400ddec3428103a6fadc07256711f49f
TheSadMuffinMan <albusgithub@mevacy.com>
2023-05-17 11:21:29 -0600

    Metronome complete

---
+++ 4_kattis/8_metronome.cpp
@@ -0,0 +1,18 @@
+/*
+Takes a number and divides it by four lol
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    float input, output;
+
+    cin >> input;
+    output = (input/4);
+
+    cout << output << endl;
+
+    return 0;
+}
