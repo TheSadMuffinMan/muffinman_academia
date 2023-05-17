d258bacc3a35ca125a14588f55bbcdc09703e1ab
TheSadMuffinMan <albusgithub@mevacy.com>
2023-05-17 11:58:25 -0600

    Jack O Lantern Juxtaposition complete

---
+++ 4_kattis/11_jackOLanternJuxtaposition.cpp
@@ -0,0 +1,19 @@
+/*
+Multiplies three numbers...
+First try
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+    int n1, n2, n3, total;
+    cin >> n1 >> n2 >> n3;
+
+    total = (n1 * n2 * n3);
+
+    cout << total;
+    return 0;
+}
