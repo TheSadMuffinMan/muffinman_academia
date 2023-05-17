9e0e932a03c9ae6c9d148f8875eff48a13de7261
TheSadMuffinMan <albusgithub@mevacy.com>
2023-05-17 11:16:17 -0600

    Quadrant selection complete

--- 4_kattis/7_quadrantSelection.cpp
+++ 4_kattis/7_quadrantSelection.cpp
@@ -1,5 +1,6 @@
 /*
 Determines what quadrant an x,y point is in
+This one took me too long lol
 */
 
 #include <iostream>
@@ -10,15 +11,17 @@ int main(int argc, char *argv[]) {
     int x, y;
     cin >> x >> y;
 
-    if ((x < 0) && (y < 0)) {
-        cout << "1" << endl;
+    if ((x > 0) && (y > 0)) {
+        cout << "1\n";
     } else if ((x < 0) && (y > 0)) {
-        cout << "2" << endl;
-    } else if ((x > 0) && (y > 0)) {
-        cout << "3" << endl;
+        cout << "2\n";
+    } else if ((x < 0) && (y < 0)) {
+        cout << "3\n";
+    } else if ((x > 0) && (y < 0)) {
+        cout << "4\n";
     } else {
-        cout << "4" << endl;
+        cout << "ERROR\n";
     }
-    
+
     return 0;
 }
