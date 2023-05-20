423e8472963a83844593f5a2d87f88c5c7ec704c
TheSadMuffinMan <albusgithub@mevacy.com>
2023-05-20 10:56:58 -0600

    This makes me mad it works, the other was better

--- 4_kattis/12_greeeetings.cpp
+++ 4_kattis/12_greeeetings.cpp
@@ -1,4 +1,5 @@
 /*
+12_greeeetings.cpp
 Takes in a phrase and doubles the number of 'e's inside it.
 Explores the string.insert() function and loop functionality
 */
@@ -8,19 +9,22 @@ Explores the string.insert() function and loop functionality
 using namespace std;
 
 int main(int argc, char *argv[]) {
-
     string input;
     cin >> input;
 
-    int length = input.length(); // This eliminates warning messages
+    cout << "h";
+    int length = (input.length() - 2); // This eliminates warning messages, and eliminates the 'h' and 'y' chars
     for (int i = 0; i < length; i++) {
-        if(input[i] == 'e') {
-            input.insert(i, "e"); // It is important to note that the insert() function uses ", even when only searching for a char
-            i++;
-        }
+        cout << "ee";
     }
-
-    cout << input << endl;
+    cout << "y\n";
 
     return 0;
 }
+
+/*
+This is the smart way of doing it, but Kattis is Kattis, so...
+        if(input[i] == 'e') {
+            input.insert(i, "e"); // It is important to note that the insert() function uses ", even when only searching for a char
+            i++;
+*/
