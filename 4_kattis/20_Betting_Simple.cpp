4f3ed4fa54539a5f423975b30a9f4fe3d7a4cc97
TheSadMuffinMan <albusgithub@mevacy.com>
2023-05-20 13:55:12 -0600

    Framework

---
+++ 4_kattis/20_Betting_Simple.cpp
@@ -0,0 +1,16 @@
+/*
+Uses ratios to break apart floats
+OPTION ONE- 1 : 1.25
+OPTION TWO- 1 : 5
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    float input;
+    cin >> input;
+
+    return 0;
+}
