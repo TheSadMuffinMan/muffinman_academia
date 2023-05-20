f41b6942c3b32db4168f4598c0413e4a7c287609
TheSadMuffinMan <albusgithub@mevacy.com>
2023-05-20 09:18:57 -0600

    FYI start

---
+++ 4_kattis/13_FYI.cpp
@@ -0,0 +1,12 @@
+/*
+Determines where to "route" a call based off first three nums of input
+Explores find() functionality
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
