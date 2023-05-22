e304c888077b0489ba0a5db8c322212b38934e84
TheSadMuffinMan <albusgithub@mevacy.com>
2023-05-22 07:49:36 -0600

    Setup

---
+++ 4_kattis/21_BatterUp.cpp
@@ -0,0 +1,13 @@
+/*
+Uses ratios to break apart floats
+https://open.kattis.com/problems/batterup
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    
+    return 0;
+}
