7352def199f63be7bbd1a9d68b0df40666efb64c
TheSadMuffinMan <albusgithub@mevacy.com>
2023-05-22 11:42:37 -0600

    Working

--- 4_kattis/21_BatterUp.cpp
+++ 4_kattis/21_BatterUp.cpp
@@ -1,5 +1,5 @@
 /*
-Uses ratios to break apart floats
+Creates an array, populates the array, and then find the average of all values (more or less)
 https://open.kattis.com/problems/batterup
 */
 
@@ -8,6 +8,20 @@ https://open.kattis.com/problems/batterup
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    
+
+    int arraySize;
+    cin >> arraySize;
+
+    float inputs[arraySize];
+    float runningNum = 0;
+
+    for (int i = 0; i < arraySize; i++) {
+        cin >> inputs[i];
+        runningNum = (runningNum + inputs[i]);
+    }
+
+    float displayNum = runningNum/arraySize;
+    cout << displayNum; 
+
     return 0;
 }
