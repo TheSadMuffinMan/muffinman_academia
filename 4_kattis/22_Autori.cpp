8e7c5abc6fe153bff2e5093f28cf6a1930ae9b2a
TheSadMuffinMan <albusgithub@mevacy.com>
2023-05-24 09:04:48 -0600

    Autotori setup and small work

---
+++ 4_kattis/22_Autori.cpp
@@ -0,0 +1,21 @@
+/*
+Program takes in names seperated by hyphens, and then returns the first chars of their names combined together
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    string input;
+    getline(cin, input);
+
+    int hypenIndex;
+    char findChar = '-';
+    
+    for (int i = 0; i < input.length(); i++) {
+
+    }
+    return 0;
+}
