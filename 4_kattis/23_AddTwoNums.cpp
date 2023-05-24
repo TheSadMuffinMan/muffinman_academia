4f5f5084ba0029ff5fb3dead83f6fedcd17bcc81
TheSadMuffinMan <albusgithub@mevacy.com>
2023-05-24 09:55:07 -0600

    Complete

---
+++ 4_kattis/23_AddTwoNums.cpp
@@ -0,0 +1,16 @@
+/*
+Adds two ints together
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int n1, n2;
+    cin >> n1 >> n2;
+
+    cout << n1 + n2;
+
+    return 0;
+}
