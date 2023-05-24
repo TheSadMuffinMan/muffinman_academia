f3d47c639b142404fcee665d3acab01baded4f69
TheSadMuffinMan <albusgithub@mevacy.com>
2023-05-24 09:52:29 -0600

    Autori complete

--- 4_kattis/22_Autori.cpp
+++ 4_kattis/22_Autori.cpp
@@ -1,9 +1,9 @@
 /*
 Program takes in names seperated by hyphens, and then returns the first chars of their names combined together
+Uses a loop to search a string for hyphens, then adds the char following it to the output
 */
 
 #include <iostream>
-#include <string>
 
 using namespace std;
 
@@ -11,11 +11,16 @@ int main(int argc, char *argv[]) {
     string input;
     getline(cin, input);
 
-    int hypenIndex;
-    char findChar = '-';
-    
-    for (int i = 0; i < input.length(); i++) {
+    string output = input.substr(0, 1);
+    size_t loopLength = input.length();
 
+    for (size_t i = 0; i < loopLength; i++) {
+        if (input[i] == '-') {
+            output = (output + (input[i +1]));
+        }
     }
+    
+    cout << output << endl;
+
     return 0;
 }
