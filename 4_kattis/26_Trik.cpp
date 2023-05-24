93d0a1a7f91ddaa1b6293ca227a25dae2852a8a7
TheSadMuffinMan <albusgithub@mevacy.com>
2023-05-24 14:03:25 -0600

    Somewhat working

--- 4_kattis/26_Trik.cpp
+++ 4_kattis/26_Trik.cpp
@@ -1,4 +1,13 @@
 /*
+Replicates the "Ball Under Cup" game, with three cups
+Ball starts under 1
+
+MOVES:
+1     2     3
+<-----><---->
+   A      B
+<----------->
+      C
 */
 
 #include <iostream>
@@ -6,5 +15,29 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
+    size_t size;
+    int ballPosition = 0;
+
+    cin >> size;
+    char userMove[size];
+    cin >> userMove[size];
+
+    cout << "userMove[0]: " << userMove[0] << endl;
+    cout << "userMove[1]: " << userMove[1] << endl;
+
+    for (size_t i = 0; i <= size; i++) {
+        if (userMove[i] == 'A') {
+            ballPosition = 1;
+        } else if (userMove[i] == 'B') {
+            ballPosition = 2;
+        } else if (userMove[i] == 'C') {
+            ballPosition = 0;
+        } else {
+            cout << "E\n";
+        }
+    }
+
+    cout << "ballPosition: " << ballPosition << endl;
+
     return 0;
 }
