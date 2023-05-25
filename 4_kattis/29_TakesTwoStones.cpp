b852dd1892c11aebf5838a1da1c9a1802e0eef6d
TheSadMuffinMan <albusgithub@mevacy.com>
2023-05-25 11:58:03 -0600

    TakesTwoStones complete

--- 4_kattis/29_TakesTwoStones.cpp
+++ 4_kattis/29_TakesTwoStones.cpp
@@ -8,5 +8,16 @@ https://open.kattis.com/problems/twostones
 using namespace std;
 
 int main(int argc, char *argv[]) {
+    int input;
+    cin >> input;
+
+    if ((input % 2) == 1) {
+        cout << "Alice" << endl;
+    } else if ((input % 2) == 0) {
+        cout << "Bob" << endl;
+    } else {
+        cout << "ERROR" << endl;
+    }
+
     return 0;
 }
