74fbf91f56f1c0844359157f98476b4cc9e70e36
TheSadMuffinMan <albusgithub@mevacy.com>
2023-05-25 11:51:35 -0600

    Complete

--- 4_kattis/28_Tarifa.cpp
+++ 4_kattis/28_Tarifa.cpp
@@ -9,10 +9,20 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
     int rate, timePeriod, total;
+    int temp1 = 0;
+    int temp2 = 0;
 
     cin >> rate >> timePeriod;
 
     total = (rate * (timePeriod + 1));
+
+    size_t size = timePeriod;
+    for (size_t i = 0; i < size; i++) {
+        cin >> temp1;
+        temp2 = (temp2 + temp1);
+    }
+
+    cout << (total - temp2) << endl;
 
     return 0;
 }
