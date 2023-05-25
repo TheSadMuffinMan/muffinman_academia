5fb0b8e78e357c38f21233847ba7071be7798276
TheSadMuffinMan <albusgithub@mevacy.com>
2023-05-25 12:42:44 -0600

    Different approach

--- 4_kattis/31_Stopwatch.cpp
+++ 4_kattis/31_Stopwatch.cpp
@@ -6,17 +6,34 @@ https://open.kattis.com/problems/stopwatch
 
 using namespace std;
 
+void fillInput(int&[], size_t);
+void calcTime(int[], size_t, int&);
+
 int main(int argc, char *argv[]) {
-    int buttonPresses, temp;
+    int buttonPresses;
     cin >> buttonPresses;
+    size_t size = buttonPresses;
 
     int runningTime = 0;
-    size_t size = buttonPresses;
     int userArray[size];
-    
-    for (size_t i = 0; i < size; i+2) {
-        cin >> userArray[i] >> userArray[i+1];
-        temp = (userArray[i] + userArray[i+1]);
+
+    if ((buttonPresses % 2) == 1) {
+        fillInput(userArray, size);
+        calcTime(userArray, size, runningTime);
+        cout << runningTime << endl;
+    } else {
+        cout << "still running" << endl;
     }
+
     return 0;
+}
+
+void fillInput(int &userArray[], size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        cin >> userArray[i];
+    }
+}
+
+void calcTime(int userArray[], size_t size, int &runningTime) {
+    int temp;
 }
