cd1114f38abeed2c9fd36148eb4daa9446bf1e72
TheSadMuffinMan <albusgithub@mevacy.com>
2023-05-25 11:53:57 -0600

    Framework

---
+++ 4_kattis/29_TakesTwoStones.cpp
@@ -0,0 +1,12 @@
+/*
+Program outputs a string based off numerical input
+https://open.kattis.com/problems/twostones
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
