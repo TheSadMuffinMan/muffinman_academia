68a1235afdb0a456fcb07a1bb371cd7b82ffe3ac
TheSadMuffinMan <albusgithub@mevacy.com>
2023-08-30 16:07:55 -0600

    Baseline things!

---
+++ 1. CS2 Data Structures/1. Notes/1. Skeleton Code/baseline_code.cpp
@@ -0,0 +1,16 @@
+/*
+Name: Anthony Streich
+Date: 
+NAME/FUNCTION OF PROGRAM
+CURRENT ISSUES: 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char *argv[]) {
+    
+    return 0;
+}
