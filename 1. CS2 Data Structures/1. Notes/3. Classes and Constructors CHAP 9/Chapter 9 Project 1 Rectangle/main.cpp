fcb469f6a772da4d36c8b1d25571661e43d5b7a7
TheSadMuffinMan <albusgithub@mevacy.com>
2023-09-02 10:02:20 -0600

    Semantics

---
+++ 1. CS2 Data Structures/1. Notes/3. Classes and Constructors CHAP 9/Chapter 9 Project 1 Rectangle/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream> // Exercise09_01
+using namespace std;
+
+class Rectangle // Define the header here
+{
+  //
+};
+
+// Implement the header here
+
+int main() {
+  // Write the test code here
+
+  return 0;
+}
