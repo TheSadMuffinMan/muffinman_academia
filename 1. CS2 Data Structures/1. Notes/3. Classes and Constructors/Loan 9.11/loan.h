7316bfab7a3b30bce5939d4bfc206b46c55fb97d
TheSadMuffinMan <albusgithub@mevacy.com>
2023-09-01 18:31:29 -0600

    9.11 baseline work

---
+++ 1. CS2 Data Structures/1. Notes/3. Classes and Constructors/Loan 9.11/loan.h
@@ -0,0 +1,24 @@
+#ifndef LOAN_H
+#define LOAN_H
+
+class Loan
+{
+public: // This is our "public interface". The user has access to all of this functionality.
+    Loan();
+    Loan(double rate, int years, double amount);
+    double getAnnualInterestRate();
+    int getNumberOfYears();
+    double getLoanAmount();
+    void setAnnualInterestRate(double rate); // Function protype
+    void setNumberOfYears(int years); // Function prototype. Function is defined in a source file.
+    void setLoanAmount(double amount); // Function prototype. Function is defined in a source file.
+    double getMonthlyPayment();
+    double getTotalPayment();
+
+private: // We don't want the user to have direct access to these data fields.
+    double annualInterestRate;
+    int numberOfYears;
+    double loanAmount;
+};
+
+#endif
