b0b6c17aa26d6d122b353d110cd40895587bd63f
TheSadMuffinMan <albusgithub@mevacy.com>
2023-09-02 10:57:42 -0600

    Completely working :O

--- 1. CS2 Data Structures/1. Notes/3. Classes and Constructors CHAP 9/Chapter 9 Project 1 Rectangle/main.cpp
+++ 1. CS2 Data Structures/1. Notes/3. Classes and Constructors CHAP 9/Chapter 9 Project 1 Rectangle/main.cpp
@@ -25,7 +25,7 @@ Rectangle::Rectangle() // Our no-arg constructor implementation
   height = 1.0;
 }
 
-Rectangle::Rectangle(double userWidth, double userHeight) // User defined constructor
+Rectangle::Rectangle(double userWidth, double userHeight) // Constructor that uses user defined values
 {
   width = userWidth;
   height = userHeight;
@@ -69,13 +69,22 @@ int main() {
   // Write the test code here
   double userWidth, userHeight;
 
-  cout << "Enter width: ";
-  cin >> userWidth;
-
-  cout << "Enter height: ";
-  cin >> userHeight;
-
+  userWidth = 4;
+  userHeight = 40;
   Rectangle rectangle1(userWidth, userHeight);
+  cout << "Rectangle 1 width: " << rectangle1.getWidth() << endl;
+  cout << "Rectangle 1 height: " << rectangle1.getHeight() << endl;
+  cout << "Rectangle 1 area: " << rectangle1.getArea() << endl;
+  cout << "Rectangle 1 perimeter: " << rectangle1.getPerimeter() << "\n" << endl;
+
+  // Changing the userWidth and userHeight for rectangle2
+  userWidth = 3.5;
+  userHeight = 35.9;
+  Rectangle rectangle2(userWidth, userHeight);
+  cout << "Rectangle 2 width: " << rectangle2.getWidth() << endl;
+  cout << "Rectangle 2 height: " << rectangle2.getHeight() << endl;
+  cout << "Rectangle 2 area: " << rectangle2.getArea() << endl;
+  cout << "Rectangle 2 perimeter: " << rectangle2.getPerimeter() <<endl;
 
   return 0;
 }
