d75a36db7c6ac8a60f3463fedd7f1ad9cb0c0382
TheSadMuffinMan <albusgithub@mevacy.com>
2023-09-03 17:37:26 -0600

    coding away

---
+++ 1. CS2 Data Structures/1. Notes/4. Strings CHAPTER 10/String Functionality/3. Erase Empty At Functions/main.cpp
@@ -0,0 +1,17 @@
+// <string> .at() .erase() .empty()
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    string s1("Welcome");
+    cout << s1.at(3) << endl; // s1.at(3) returns 'c'
+    cout << s1.erase(2,3) << endl; // s1 is now "Weme"
+
+    s1.clear(); // s1 is now empty
+    cout << s1.empty() << endl; // s1.empty returns '1' (means statement is true)
+    
+    return 0;
+}
