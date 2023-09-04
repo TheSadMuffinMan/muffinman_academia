6cc6016d1dde0216a6d431367cc38902bfff6d9a
TheSadMuffinMan <albusgithub@mevacy.com>
2023-09-03 18:30:10 -0600

    Find function

---
+++ 1. CS2 Data Structures/1. Notes/4. Strings CHAPTER 10/String Functions/7. Find Function/find.cpp
@@ -0,0 +1,20 @@
+// <string> find()
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    string s1("Welcome to HTML");
+
+    cout << s1.find("co") << endl; // Returns 3
+    cout << s1.find("co", 6) << endl; // Returns string::npos
+    cout << s1.find('o') << endl; // Returns 4
+    cout << s1.find('o', 6) << endl; // Returns 9
+
+    string s("abcdefag");
+    cout << s.find("def") << " " << s.find("a", 3);
+
+    return 0;
+}
