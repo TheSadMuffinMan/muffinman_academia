f40b33b3fb73efa17c1e816e524379b7fbc3172e
TheSadMuffinMan <albusgithub@mevacy.com>
2023-09-03 19:54:33 -0600

    Quiz time

---
+++ 1. CS2 Data Structures/1. Notes/4. Strings CHAPTER 10/3. Book Examples/1. Quizzes/Quiz 10.2/quiz_10_2_2.cpp
@@ -0,0 +1,16 @@
+/* ***QUESTION 2***
+Assume that s is a string. Write an expression whose value is true if and only if
+the value of s would come between "mortgage" and "mortuary" in the dictionary.
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    string s;
+
+    return 0;
+}
