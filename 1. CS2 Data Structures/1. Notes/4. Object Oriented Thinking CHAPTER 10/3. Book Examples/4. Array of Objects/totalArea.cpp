003cd94262e7edfee658cb5461713c5ad3301645
TheSadMuffinMan <albusgithub@mevacy.com>
2023-09-04 10:24:55 -0600

    Code documentation

--- 1. CS2 Data Structures/1. Notes/4. Object Oriented Thinking CHAPTER 10/3. Book Examples/4. Array of Objects/totalArea.cpp
+++ 1. CS2 Data Structures/1. Notes/4. Object Oriented Thinking CHAPTER 10/3. Book Examples/4. Array of Objects/totalArea.cpp
@@ -1,4 +1,7 @@
 // Program outputs the area of a circle of each n radius, up to n ints.
+// Program then returns the sum of all areas
+// Program then dynamically fits all returned data in a neat format
+// Program is essentially a radius factorial generator :)
 
 #include <iostream>
 #include <iomanip>
@@ -6,7 +9,7 @@
 using namespace std;
 
 // Add circle areas
-double sum(Circle circleArray [], int size)
+double sum(Circle circleArray[], int size)
 {
   // Initialize sum
   double sum = 0;
@@ -24,15 +27,13 @@ void printCircleArray(Circle circleArray[], int size)
   cout << setw(35) << left << "Radius" << setw(8) << "Area" << endl;
   for (int i = 0; i < size; i++)
   {
-    cout << setw(35) << left << circleArray[i].getRadius() 
-      << setw(8) << circleArray[i].getArea() << endl;
+    cout << setw(35) << left << circleArray[i].getRadius() << setw(8) << circleArray[i].getArea() << endl;
   }
 
   cout << "-----------------------------------------" << endl;
 
   // Compute and display the result
-  cout << setw(35) << left << "The total area of circles is" 
-    << setw(8) << sum(circleArray, size) << endl;
+  cout << setw(35) << left << "The total area of circles is" << setw(8) << sum(circleArray, size) << endl;
 }
 
 int main()
