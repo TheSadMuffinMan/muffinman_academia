736233fcec495b0868033f877df0b18f82fdf00d
TheSadMuffinMan <albusgithub@mevacy.com>
2023-09-03 18:06:30 -0600

    Coding away

---
+++ 1. CS2 Data Structures/1. Notes/4. Strings CHAPTER 10/String Functions/5. Compare Function/compare.cpp
@@ -0,0 +1,20 @@
+// <string> compare
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    string s1("Welcome");
+    cout << s1.length() << endl; // Length is 7
+    cout << s1.size() << endl; // Size is 7
+    cout << s1.capacity() << endl; // Capacity is 15
+
+    s1.erase(1, 2);
+    cout << s1.length() << endl; // Length is now 5
+    cout << s1.size() << endl; // Size is now 5
+    cout << s1.capacity() << endl; // Capacity is still 15
+
+    return 0;
+}
