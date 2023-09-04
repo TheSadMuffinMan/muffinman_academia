0b5eefa89ff3eac66b35a6646c53485e8fde49d0
TheSadMuffinMan <albusgithub@mevacy.com>
2023-09-03 18:25:17 -0600

    Substring

---
+++ 1. CS2 Data Structures/1. Notes/4. Strings CHAPTER 10/String Functions/6. Substring Functions/substring.cpp
@@ -0,0 +1,16 @@
+// <string> substr() and .at()
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    string s1("Welcome");
+
+    cout << s1.substr(0, 1) << endl; // Returns 'W'
+    cout << s1.substr(3) << endl; // Returns "come"
+    cout << s1.substr(3, 3) << endl; // Returns "com"
+
+    return 0;
+}
