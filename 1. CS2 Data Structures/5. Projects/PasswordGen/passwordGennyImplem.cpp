a18f6d88fd0e5f3b44aa2158ed276a35028f39c1
TheSadMuffinMan <albusgithub@mevacy.com>
2023-09-26 16:51:39 -0600

    Fixed but cheated

--- 1. CS2 Data Structures/5. Projects/PasswordGen/passwordGennyImplem.cpp
+++ 1. CS2 Data Structures/5. Projects/PasswordGen/passwordGennyImplem.cpp
@@ -5,16 +5,19 @@ int cleanedPWSize(int &pwSize)
     while (true)
     {
         std::cout << "Loop pwSize: ";
-        std::cin >> pwSize;
-
-        if (pwSize >= 1 && pwSize <= 21)
+        if (!(std::cin >> pwSize))
+        {
+            std::cout << "Invalid input. Try again: " << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore the rest of the input line
+        }
+        else if (pwSize >= 1 && pwSize <= 21)
         {
             break;
         }
         else
         {
-            std::cout << "Invalid pwSize. Try again: ";
-            std::cin >> pwSize;
+            std::cout << "Invalid size. Try again: ";
         }
     }
 
