5294f393c047f50b8c58fd8c8be5a965681f127f
muffinMan <anthony.streich@gmail.com>
2023-10-23 17:32:32 -0600

    Slow and steady work

---
+++ 1. CS2 Data Structures/5. Projects/PasswordManager/passwordManagerHeader.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <random>
+
+class passwordObject
+{
+public:
+    passwordObject(); // Default constructor
+    passwordObject(std::string); // Builds our password object
+    ~passwordObject();
+
+    // Getters
+    std::string getPasswordName();
+
+    // Setters
+    void setPasswordName(std::string);
+
+private:
+    std::string actualPassword;
+};
