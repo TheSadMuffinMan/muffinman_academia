75329cd990cda388e8497574f4402b75da3cebf0
muffinMan <anthony.streich@gmail.com>
2024-01-04 15:12:10 -0700

    Old coding

---
+++ 1. CS2 Data Structures/5. Projects/PasswordManagerv2/PasswordObject.h
@@ -0,0 +1,24 @@
+#pragma once
+
+class passwordObject
+{
+public:
+    passwordObject(); // Default constructor
+    ~passwordObject(); // Destructor
+    passwordObject(std::string, std::string, std::string, std::string); // Builds our password object
+
+    // Getters
+    std::string getLabel();
+    std::string getPassword();
+    std::string getUserName();
+    std::string getComment();
+
+    // Setters
+    void setLabel(std::string);
+    void setPassword(std::string);
+    void setUserName(std::string);
+    void setComment(std::string);
+
+private:
+    std::string actualLabel, actualUsername, actualPassword, actualComment;
+};
---
+++ 1. CS2 Data Structures/5. Projects/PasswordManagerv2/mainPWManager.cpp
@@ -0,0 +1,10 @@
+#include "PWManager.h"
+#include "PasswordObject.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    //
+    return 0;
+}
