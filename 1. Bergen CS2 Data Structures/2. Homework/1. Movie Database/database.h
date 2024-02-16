c04a849b2dc8ddbd7c8a764b5bd045f191283f8a
muffinMan <anthony.streich@gmail.com>
2024-02-16 12:54:47 -0700

    Classwork

--- 1. Bergen CS2 Data Structures/2. Homework/1. Movie Database/database.h
+++ 1. Bergen CS2 Data Structures/2. Homework/1. Movie Database/database.h
@@ -3,7 +3,7 @@
 #include "movie.h"
 
 // The database should have the following variables: name, db_id, array for Movies (as pointers)
-// Add movie, remove movie (given ID), display all movies, search for movie given title or genre
+// Functionality: Add movie, remove movie (given ID), display all movies, search for movie given title or genre
 // and print out a table of them.
 class Database
 {
@@ -13,6 +13,6 @@ class Database
     private:
         std::string name;
         int db_id;
-        // std::size_t movieArraySize = 100;
+        int movieListSize;
         movieNamespace::MovieClass* movieListArray[];
 };
