1e7bc26704502df95a5471335b8307eb3baf9132
muffinMan <anthony.streich@gmail.com>
2024-04-01 12:47:12 -0600

    Woh stacks are freakin cool

---
+++ 1. Bergen CS2 Data Structures/1. Lecture/4.0 Stacks/main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+
+using namespace std;
+
+class Stack
+{
+    public:
+        Stack();
+
+        bool empty();
+        int size();
+
+        void push(int);
+        int pop();
+        int peek();
+    
+    private:
+        int stack[100];
+        int top;
+};
+
+int main(int argc, char* argv[])
+{
+    Stack myStack;
+
+    myStack.push(42);
+    myStack.push(15);
+    myStack.push(9000);
+    myStack.push(-3);
+
+    while (myStack.empty() == false)
+    {
+        cout << myStack.pop() << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+Stack::Stack()
+{
+    top = -1;
+}
+
+bool Stack::empty()
+{
+    if (top == -1) return true;
+    return false;
+}
+
+int Stack::size()
+{
+    return top + 1;
+}
+
+void Stack::push(int data)
+{
+    /* This code would be implemented if you needed an array larger than 100 data members.
+    if (top == 99)
+    {
+        // Create larger array and copy elements.
+    }
+    */
+    top++;
+    stack[top] = data;
+}
+
+int Stack::pop()
+{
+    if (empty())
+    {
+        cerr << "Stack is empty." << endl;
+        return -999;
+    }
+
+    top--;
+    return stack[top+1];
+}
+
+int Stack::peek()
+{
+    if (empty())
+    {
+        cerr << "Stack is empty." << endl;
+        return -999;
+    }
+
+    return stack[top];
+}
