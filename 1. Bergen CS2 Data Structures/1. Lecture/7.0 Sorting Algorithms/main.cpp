99c1ebfcd5aabf3d35d5a31e920dc3111a2c539e
muffinMan <anthony.streich@gmail.com>
2024-04-11 12:52:22 -0600

    Lecture

--- 1. Bergen CS2 Data Structures/1. Lecture/7.0 Sorting Algorithms/main.cpp
+++ 1. Bergen CS2 Data Structures/1. Lecture/7.0 Sorting Algorithms/main.cpp
@@ -1,4 +1,8 @@
 /*
+
+KEY CONCEPTS: <algorithm> library, <chrono> library, bubble sort, selection sort, insertion sort,
+quick sort, merge sort.
+
 // int unsortedArray[6] = {3, -4, 62, 1003, -15, 42};
 // int sortedArray[6] = {-15, -4, 3, 42, 62, 1003};
 In Place Sorting: Data is sorted before being placed into memory. Space complexity O(1).
@@ -49,11 +53,117 @@ MERGE SORT:
 */
 
 #include <iostream>
+#include <algorithm> // Allows the "swap()" function.
+#include <chrono> // Allows timing of functionality O.O
 
 using namespace std;
 
+void buildArray(int[], int);
+void printArray(int[], int);
+void copyArray(int[], int[], int);
+
+void selectionSort(int[], int);
+void insertionSort(int[], int);
+
 int main(int argc, char *argv[])
 {
-    
+    int arraySize = 100;
+    int array[arraySize];
+
+    buildArray(array, arraySize);
+
     return 0;
 }
+
+void buildArray(int array[], int arraySize)
+{
+    srand(time(0));
+    for (int i = 0; i < arraySize; i++)
+    {
+        array[i] = rand();
+    }
+}
+
+
+void printArray(int array[], int arraySize)
+{
+    for (int i = 0; i < arraySize; i++)
+    {
+        cout << array[i] << " ";
+    }
+}
+
+void copyArray(int originalArray[], int copiedArray[], int arraySize)
+{
+    for (int i = 0; i < arraySize; i++)
+    {
+        copiedArray[i] = originalArray[i];
+    }
+}
+
+// Order n^2 time complex algorithm.
+void selectionSort(int array[], int arraySize)
+{
+    bool swapped = false;
+    int minimum;
+    for (int i = 0; i < arraySize - 1; i ++)
+    {
+        minimum = i;
+
+        for (int j = i + 1; j < arraySize; j++)
+        {
+            if (array[j] < array[minimum])
+            {
+                minimum = j;
+            }
+        }
+
+        if (minimum != i)
+        {
+            swap(array[minimum], array[i]);
+        }
+    }
+}
+
+// Order n^2 time complex algorithm.
+void insertionSort(int array[], int arraySize)
+{
+    for (int i = 1; i < arraySize; i++)
+    {
+        int key = array[i];
+        int j = i - 1;
+
+        while ((j >= 0) && (array[j] > key))
+        {
+            array[j + 1] = array[j];
+            j = j - 1;
+        }
+
+        array[j + 1] = key;
+    }
+}
+
+/* ***NOT WORKING***
+void bubbleSort(int[], int);
+void bubbleSort(int array[], int arraySize)
+{
+    bool swapped = false;
+
+    for(int i = 0; i < arraySize; i++)
+    {
+        swapped = false;
+        for (int j = 0; j < arraySize - 1 - i; j++)
+        {
+            if (array[j], array[j+1])
+            {
+                swap(array[j], array[j+1]);
+                swapped = true;
+            }
+        }
+        // if (!swapped)
+        // {
+
+        // }
+    }
+}
+*/
