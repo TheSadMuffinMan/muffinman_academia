ac0fa5737e98ea3e2064e8251fc2537f12095f29
muffinMan <anthony.streich@gmail.com>
2024-05-09 14:26:36 -0600

    Organized everything working on minVal now

--- 1. Bergen CS2 Data Structures/2. Homework/4. FINAL PROJECT/tree.h
+++ 1. Bergen CS2 Data Structures/2. Homework/4. FINAL PROJECT/tree.h
@@ -2,7 +2,7 @@
 Builds a BST composed of Nodes (_l,_r,_data) with _data = *Word.
 word.h Completed on 5-9-24.
 
-WORKING ON: removeData
+WORKING ON: _minVal
 */
 #pragma once
 #include "node.h"
@@ -20,56 +20,52 @@ class BST
 
     private:
         Node<T1>* _root;
-        Node<T1>* insertNode(Node<T1>*, T1);
-        void inOrderPrint(Node<T1>*);
-        Node<T1>* searchData(Node<T1>*, T1);
-        Node<T1>* removeData(Node<T1>*, T1); // In progress
-        Node<T1>* minVal(Node<T1>*); // Working on
+        Node<T1>* _insertNode(Node<T1>*, T1);
+        void _inOrderPrint(Node<T1>*);
+        Node<T1>* _searchData(Node<T1>*, T1);
+        Node<T1>* _removeData(Node<T1>*, T1); // In progress
+        Node<T1>* _minVal(Node<T1>*); // Working on
 };
 
-// Destructor, should check if root exists and then delete it. Each node will handle deleting its leaf nodes.
+/*
+Bergen: { Destructor, should check if root exists and then delete it.
+Each node will handle deleting its leaf nodes.
+} */
 template <class T1>
 BST<T1>::~BST()
 {
-    if (_root)
-    {
-        //
-    }
+    //
 }
 
 /*
-Bergen: {
-    Public method that takes in some data and passes that into the removeData method.
-    Don't forget to update _root as you may end up deleting the original root.
-}
-*/
+Bergen: { Public method that takes in some data and passes that into the _removeData method.
+Don't forget to update _root as you may end up deleting the original root.
+} */
 template <class T1>
 void BST<T1>::remove(T1 data)
 {
 }
 
 /*
-Bergen: {
-    Private method to recursively walk the tree until the data is found.
-    Then using the process built in class, check if there is only one leaf, if so then move that leaf into the
-        root position.
-    If there are two leafs, find the smallest value in the right subtree (or the largest value in the left subtree
-        but you would have to rename minVal method) and then replace the current node with that.
-    Don't forget to clean up your memory!
-    }
-*/
+Bergen: { Private method to recursively walk the tree until the data is found.
+Then using the process built in class, check if there is only one leaf, if so then move that leaf into the
+    root position.
+If there are two leafs, find the smallest value in the right subtree (or the largest value in the left subtree
+    but you would have to rename _minVal method) and then replace the current node with that.
+Don't forget to clean up your memory!
+} */
 template <class T1>
-Node<T1>* BST<T1>::removeData(Node<T1>* root, T1 data)
+Node<T1>* BST<T1>::_removeData(Node<T1>* root, T1 data)
 {
     // The below conditionals are my BASE CASE(s).
     // The conditionals traverse the tree until passed data == (passed)root->data (data is found in BST).
     if (data > root->getData())
     {
-        return removeData(root->getRight(), data);
+        return _removeData(root->getRight(), data);
     }
     else if (data < root->getData())
     {
-        return removeData(root->getLeft(), data);
+        return _removeData(root->getLeft(), data);
     }
 
     // == When we find the data to delete...
@@ -97,12 +93,10 @@ Node<T1>* BST<T1>::removeData(Node<T1>* root, T1 data)
 }
 
 /*
-Bergen: {
-    Given a node, find the smallest value in that subtree. Return that node.
-}
-*/
+Bergen: { Given a node, find the smallest value in that subtree. Return that node.
+} */
 template <class T1>
-Node<T1>* BST<T1>::minVal(Node<T1>* root)
+Node<T1>* BST<T1>::_minVal(Node<T1>* root)
 {
     if (root->getLeft() == nullptr)
     {
@@ -110,24 +104,24 @@ Node<T1>* BST<T1>::minVal(Node<T1>* root)
     }
     else if (root->getRight() != nullptr)
     {
-        return minVal(root->getRight());
+        return _minVal(root->getRight());
     }
     return nullptr;
 }
 
-// Given data and a node, recursively walk the tree to find that node if it exists.
+/*
+Bergen: { Given data and a node, recursively walk the tree to find that node if it exists.
+} */
 template <class T1>
-Node<T1>* BST<T1>::searchData(Node<T1>* root, T1 data)
+Node<T1>* BST<T1>::_searchData(Node<T1>* root, T1 data)
 {
     return nullptr;
 }
 
 /*
-Bergen: {
-    Wrapper function for searchData. Pass data, root into searchData and return true if data found, return
-        false if data not found.
-}
-*/
+Bergen: { Wrapper function for _searchData. Pass data, root into _searchData and return true if data found,
+    return false if data not found.
+} */
 template <class T1>
 bool BST<T1>::search(T1 data)
 {
@@ -135,55 +129,49 @@ bool BST<T1>::search(T1 data)
 }
 
 /*
-Bergen: {
-    Given a node, recursively walk the tree to print out the inOrder format. That's left->root->right.
-    Make sure you cout with a space separating each value as I based my tests on that!
-    For example, you would end up cout (do not add the "): "1 2 3 4 5 "
-    **Notice that there is an extra space at the end**
-}
-*/
+Bergen: { Given a node, recursively walk the tree to print out the inOrder format. That's left->root->right.
+Make sure you cout with a space separating each value as I based my tests on that!
+For example, you would end up cout (do not add the "): "1 2 3 4 5 "
+**Notice that there is an extra space at the end**
+} */
 template <class T1>
-void BST<T1>::inOrderPrint(Node<T1>* root)
+void BST<T1>::_inOrderPrint(Node<T1>* root)
 {
 }
 
-// Wrapper for inOrderPrint
+/*
+Bergen: { Wrapper for _inOrderPrint.
+} */
 template <class T1>
 void BST<T1>::inOrder()
 {
 }
 
 /*
-Bergen: {
-    Give some data and a node, recursively walk the tree until you get to a nullptr and store the value there.
-    You should ensure that if data < the current node's data, you walk the left. If data > current node's data,
-        walk right.
-    You should check if the value already exists and say so (it's just adding an else with a cout).
-}
-*/
+Bergen: { Give some data and a node, recursively walk the tree until you get to a nullptr and store the value there.
+You should ensure that if data < the current node's data, you walk the left. If data > current node's data,
+    walk right.
+You should check if the value already exists and say so (it's just adding an else with a cout).
+} */
 template <class T1>
-Node<T1>* BST<T1>::insertNode(Node<T1>* root, T1 data)
+Node<T1>* BST<T1>::_insertNode(Node<T1>* root, T1 data)
 {
     return nullptr;
 }
 
 /*
-Bergen: {
-    Wrapper for insertNode. Take in data to pass that and _root to insertNode. Ensure you update _root since if
-        the tree is empty, that would be the new _root.
-}
-*/
+Bergen: { Wrapper for insertNode. Take in data to pass that and _root to insertNode. Ensure you update _root since if
+    the tree is empty, that would be the new _root.
+} */
 template <class T1>
 void BST<T1>::insert(T1 data)
 {
 }
 
 /*
-Bergen: {
-    Search for the word by calling searchData passing in _root and data.
-    If node is found, setData on that node with getData++
-}
-*/
+Bergen: { Search for the word by calling _searchData passing in _root and data.
+If node is found, setData on that node with getData++.
+} */
 template <class T1>
 void BST<T1>::increment(T1 data)
 {
