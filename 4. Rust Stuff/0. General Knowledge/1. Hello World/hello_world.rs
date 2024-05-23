66b4f5ab036a81e2641cc1a77bce9bb428b177ab
muffinMan <anthony.streich@gmail.com>
2024-05-23 15:51:06 -0600

    Hello world in Rust!!

---
+++ 4. Rust Stuff/0. General Knowledge/1. Hello World/hello_world.rs
@@ -0,0 +1,14 @@
+// Comments function in the same exact way as C++.
+fn main()
+{
+    println!("Hello world!");
+}
+
+/*
+To "run" this code, first navigate to where the program is located in your terminal.
+Compile the code by inputting "rustc <Program Name>.rs"
+    Do not be alarmed, nothing happen (if there are no errors).
+This will result in a new program being created, named <Program Name>.
+Run the program by inputting "./<Program Name>".
+Program will run!
+*/
