cfcd7a08f277802523b3bfc15c241415f2ef990c
muffinMan <anthony.streich@gmail.com>
2024-05-24 11:43:09 -0600

    Notes

--- 4. Rust Stuff/first_cargo/src/main.rs
+++ 4. Rust Stuff/first_cargo/src/main.rs
@@ -5,10 +5,21 @@ fn main()
 }
 
 /*
-To "run" this code, first navigate to where the program is located in your terminal.
-Compile the code by inputting "rustc <Program Name>.rs"
-    Do not be alarmed, nothing happen (if there are no errors).
-This will result in a new program being created, named <Program Name>.
-Run the program by inputting "./<Program Name>".
-Program will run!
+To "properly" compile and run this code, first navigate to where the program is located in your terminal.
+Instead of using the "rustc <Program Name>" command, input the "cargo build" command.
+    This command compiles and builds our executable program.
+    If you type "ls", you'll several new files.
+cd into "target" directory.
+cd into "debug" directory.
+    ./<Program Name>
+    This will run the program.
+*/
+
+/*
+Alternatively, a faster way to compile and run your code is to input "cargo run" inside of the cargo's main directory.
+*/
+
+/*
+Another very useful terminal command is "cargo check" (when inside base directory).
+    This command checks if your code will compile without actually compiling the program.
 */
