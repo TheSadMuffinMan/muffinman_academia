dd4ffdc14b7126035e576178043ace389b93d3c1
muffinMan <anthony.streich@gmail.com>
2024-05-24 12:43:37 -0600

    Data types intoduction

---
+++ 4. Rust Stuff/learning3_data_types/src/main.rs
@@ -0,0 +1,15 @@
+fn main()
+{
+    println!("Hello, world!");
+}
+
+/*
+PRIMITIVE DATA TYPES: the basic or fundamental data tyes used to declare a variable.
+    There are two types of data in Rust: SCALAR and COMPOUND types.
+SCALAR DATA TYPE: data that has a finite set of possible values, meaning that each value can be compared to
+    any other values as either =, <, or >.
+    Examples include integers, floating-point values, booleans, and chars.
+COMPOUND DATA TYPE: any data type which can be constructed in a program using the programming language's
+    primitive data types and other composite types.
+    Examples include arrays and touples.
+*/
