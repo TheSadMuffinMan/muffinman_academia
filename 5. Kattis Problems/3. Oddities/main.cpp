9220b77c230d948157d43e7fd05da16c1ff6d19a
muffinMan <anthony.streich@gmail.com>
2024-06-27 15:07:09 -0600

    Oddities complete

--- 5. Kattis Problems/3. Oddities/main.cpp
+++ 5. Kattis Problems/3. Oddities/main.cpp
@@ -1,9 +1,9 @@
 // https://open.kattis.com/problems/oddities
 /*
-Working on 6/27/24
+COMPLETE 6/27/24
 
 INPUT:
-    Input begins with an integer 1 <= n <= 20 on a line by itself, indicating the number of test cases
+    Input begins with an integer (1 <= n <= 20) on a line by itself, indicating the number of test cases
         that follow. Each of the following n lines contain a test case consisting of a single
         integer -10 <= x <= 10.
 
@@ -15,6 +15,25 @@ OUTPUT:
 
 int main(int argc, char *argv[])
 {
-    //
+    std::size_t loopSize = 0;
+    std::cin >> loopSize;
+
+    int userVar, compareVar;
+
+    for (std::size_t i = 0; i < loopSize; i++)
+    {
+        std::cin >> userVar;
+        compareVar = userVar % 2;
+
+        if ((compareVar == 1) or (compareVar == -1))
+        {
+            std::cout << userVar << " is odd" << std::endl;
+        }
+        else
+        {
+            std::cout << userVar << " is even" << std::endl;
+        }
+    }
+
     return 0;
 }
