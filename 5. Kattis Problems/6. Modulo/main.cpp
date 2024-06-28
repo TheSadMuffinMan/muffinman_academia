f7241b0e96e8d107fab91649b7261c386b52042d
muffinMan <anthony.streich@gmail.com>
2024-06-28 13:29:18 -0600

    Modulo complete

--- 5. Kattis Problems/6. Modulo/main.cpp
+++ 5. Kattis Problems/6. Modulo/main.cpp
@@ -1,6 +1,6 @@
 // https://open.kattis.com/problems/modulo
 /*
-6/28/24 working on
+6/28/24 COMPLETE
 
 INPUT:
     The input will contain 10 non-negative integers, each smaller than 1000, one per line.
@@ -16,6 +16,40 @@ OUTPUT:
 
 int main(int argc, char *argv[])
 {
-    //
+    int resultArray[10];
+    for (std::size_t k = 0; k < 10; k++) // Filling array with -1's.
+    {
+        resultArray[k] = -1;
+    }
+
+    int comparisonInt, resultInt;
+    std::size_t numUniqueInts = 0;
+
+    for (std::size_t i = 0; i < 10; i++)
+    {
+        std::cin >> comparisonInt;
+        resultInt = comparisonInt % 42;
+
+        for (std::size_t j = 0; j < 10; j++) // Dropping resultInt into resultArray[].
+        {
+            if (resultArray[j] == resultInt)
+            { // If an int is a duplicate.
+                break;
+            }
+            else if ((resultArray[j] >= 0) && (resultArray[j] < 1000))
+            { // If an int is already inside resultArray[].
+                continue;
+            }
+            else
+            {
+                resultArray[j] = resultInt;
+                numUniqueInts++;
+                break;
+            }
+        }
+    }
+
+    std::cout << numUniqueInts << std::endl;
+
     return 0;
 }
