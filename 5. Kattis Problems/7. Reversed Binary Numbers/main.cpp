616f4adccb9a1f0b355ed2298c84d88e67fa6e5d
muffinMan <anthony.streich@gmail.com>
2024-06-28 13:35:40 -0600

    Slight changes

---
+++ 5. Kattis Problems/7. Reversed Binary Numbers/main.cpp
@@ -0,0 +1,22 @@
+// https://open.kattis.com/problems/reversebinary
+/*
+6/28/24 working on
+
+TASK:
+    Your task will be to write a program for reversing numbers in binary. For instance, the binary
+        representation of 13 is 1101, and reversing it gives 1011, which corresponds to number 11.
+
+INPUT:
+    /
+
+OUTPUT:
+    /
+*/
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    //
+    return 0;
+}
