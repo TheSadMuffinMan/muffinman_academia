6c1414d346fe6577b33f187ed05d82a4848d5a9a
muffinMan <anthony.streich@gmail.com>
2024-06-28 13:57:46 -0600

    Working on

--- 5. Kattis Problems/7. Reversed Binary Numbers/main.cpp
+++ 5. Kattis Problems/7. Reversed Binary Numbers/main.cpp
@@ -1,22 +1,36 @@
 // https://open.kattis.com/problems/reversebinary
 /*
-6/28/24 working on
+6/28/24 working on. This one is difficult.
 
 TASK:
     Your task will be to write a program for reversing numbers in binary. For instance, the binary
         representation of 13 is 1101, and reversing it gives 1011, which corresponds to number 11.
 
 INPUT:
-    /
+    The input contains a single line with an integer N(1 <= N <= 1,000,000,000).
 
 OUTPUT:
-    /
+    Output one line with one integer, the number we get by reversing the binary representation of N.
 */
 
 #include <iostream>
+#include <string>
 
 int main(int argc, char *argv[])
 {
-    //
+    int inputInt;
+    std::cin >> inputInt;
+
+    int arr[64];
+    int i = 0;
+    int r;
+
+    while (inputInt != 0)
+    {
+        r = inputInt % 2;
+        arr[i++] = r;
+        inputInt /= 2;
+    }
+
     return 0;
 }
