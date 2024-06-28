716d8139b8d461ffbee265c5091d77eaebb8b1ab
muffinMan <anthony.streich@gmail.com>
2024-06-28 11:32:03 -0600

    Fully complete

--- 5. Kattis Problems/5. Keys Phone Wallet/main.cpp
+++ 5. Kattis Problems/5. Keys Phone Wallet/main.cpp
@@ -1,6 +1,6 @@
 // https://open.kattis.com/problems/keysphonewallet
 /*
-6/28/24 working on
+6/28/24 COMPLETE, very first compilation worked :D
 
 INPUT:
     The first line of input contains a single integer N(1 <= N <= 20) indicating the number of items you
@@ -14,9 +14,55 @@ OUTPUT:
 */
 
 #include <iostream>
+#include <string>
+#include <sstream>
 
 int main(int argc, char *argv[])
 {
-    //
+    std::size_t numItems = 0;
+    std::string comparisonString = " ";
+    bool hasKeys = false; bool hasPhone = false; bool hasWallet = false;
+
+    std::cin >> numItems;
+
+    for (std::size_t i = 0; i < numItems; i++)
+    {
+        std::cin >> comparisonString;
+        if (comparisonString == "keys")
+        {
+            hasKeys = true;
+        }
+        else if (comparisonString == "phone")
+        {
+            hasPhone = true;
+        }
+        else if (comparisonString == "wallet")
+        {
+            hasWallet = true;
+        }
+        else {continue;}
+    }
+
+    if ((hasKeys == true) && (hasPhone == true) && (hasWallet == true))
+    {
+        std::cout << "ready";
+        return 0;
+    }
+
+    if (hasKeys == false)
+    {
+        std::cout << "keys" << std::endl;
+    }
+
+    if (hasPhone == false)
+    {
+        std::cout << "phone" << std::endl;
+    }
+
+    if (hasWallet == false)
+    {
+        std::cout << "wallet" << std::endl;
+    }
+
     return 0;
 }
