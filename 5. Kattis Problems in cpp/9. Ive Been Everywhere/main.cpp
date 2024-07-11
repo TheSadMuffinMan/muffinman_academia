de33ac674050886c53f2a798d206689012be4208
Muffinman <albusgithub@mevacy.com>
2024-07-11 16:30:22 -0600

    Nearing completion but not done

--- 5. Kattis Problems in cpp/9. Ive Been Everywhere/main.cpp
+++ 5. Kattis Problems in cpp/9. Ive Been Everywhere/main.cpp
@@ -1,6 +1,6 @@
 // https://open.kattis.com/problems/everywhere
 /*
-7/11/24 working on
+7/11/24 Kinda works, problems iterating resultArray.
 
 TASK:
     Alice travels a lot for her work. Each time she travels, she visits a single city before returning home.
@@ -16,15 +16,82 @@ INPUT:
         one letter, and do not contain spaces.
     The number of trips is at most 100 and no city name contains more than 20 characters.
 
+    Example1:
+        2
+        7
+        saskatoon
+        toronto
+        winnipeg
+        toronto
+        vancouver
+        saskatoon
+        toronto
+        3
+        edmonton
+        edmonton
+        edmonton
+
 OUTPUT:
     For each test case, simply output a single line containing a single integer that is the number of distinct cities
         that Alice has visited on her work trips.
+    
+    Example1:
+    4
+    1
 */
 
 #include <iostream>
+#include <string>
 
 int main(int argc, char *argv[])
 {
-    //
+    std::size_t numTrips = 0;
+
+    std::cin >> numTrips;
+    std::size_t resultArray[numTrips]; // Holds the # of unique cities visited per trip.
+
+    for (std::size_t i = 0; i < numTrips; i++)
+    {
+        std::size_t numCities = 0;
+        std::cin >> numCities;
+
+        std::string citiesArray[numCities];
+        for (std::size_t x = 0; x < numCities; x++) // Populating array with "x"s.
+        {
+            citiesArray[x] = "x";
+        }
+
+        for (std::size_t j = 0; j < numCities; j++) // Loop to grab each input city.
+        {
+            std::string currCity;
+            std::cin >> currCity;
+
+            if (j == 0)
+            {
+                citiesArray[0] = currCity;
+                resultArray[i] = 1;
+                continue;
+            }
+
+            for (std::size_t k = 0; k < numCities; k++) // Compares currCity to ea string inside citiesArray.
+            {
+                if (citiesArray[k] == currCity)
+                {
+                    break;
+                }
+                else if (citiesArray[k] == "x")
+                {
+                    citiesArray[k] = currCity;
+                    resultArray[i] = i + 1;
+                }
+            }
+        }
+    }
+
+    for (std::size_t p = 0; p < numTrips; p++)
+    {
+        std::cout << resultArray[p] << " ";
+    }
+
     return 0;
 }
