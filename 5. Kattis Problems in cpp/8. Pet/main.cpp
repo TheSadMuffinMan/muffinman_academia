30846ed03b1f6f91925d2764ee34c72bb93682f1
Muffinman <albusgithub@mevacy.com>
2024-07-11 14:28:47 -0600

    Pet complete!

--- 5. Kattis Problems in cpp/8. Pet/main.cpp
+++ 5. Kattis Problems in cpp/8. Pet/main.cpp
@@ -1,14 +1,14 @@
 // https://open.kattis.com/problems/pet
 /*
-7/11/24 Working on.
+7/11/24 COMPLETE, first try :D
 
 TASK:
     In the popular show “Dinner for Five”, five contestants compete in preparing culinary delights. Every
         evening one of them makes dinner and each of other four then grades it on a scale from 1 to 5. The
         number of points a contestant gets is equal to the sum of grades they got. The winner of the show
-        is of course the contestant that gets the most points.
+        is the contestant that gets the most points.
 
-    Write a program that determines the winner and how many points they got.
+    Write a program that determines the currLargest and how many points they got.
 
 INPUT:
     Five lines, each containing 4 integers, the grades a contestant got. The contestants are numbered 1 to
@@ -22,56 +22,79 @@ INPUT:
         4 4 4 5
 
 OUTPUT:
-    Output on a single line the winner’s number and their points, separated by a space. The input data will
+    Output on a single line the currLargest’s number and their points, separated by a space. The input data will
         guarantee that the solution is unique.
 
     Example 1:
         4 19
 
 PLAN:
-    Build 5 arrays of 5 ints. First 4 ints will be input, the last will be the sum of the previous ints.
-        This will make determining the highest score very easy later in the program because we will only
-        have to compare the 5th index of each array.
+    Build 5 arrays of 6 ints. First int identifies which contestant it is, next 4 are scores, and last int is
+        the contestant's total score.
 */
 
 #include <iostream>
 
-int buildArray(int[]);
-bool largerThanRest(int);
+int buildArray(int, int[]);
+int* returnLarger(int*, int[]);
 
 int main(int argc, char *argv[])
 {
-    int array0[4], array1[4], array2[4], array3[4], array4[4];
-    int* winner = array0;
+    int array0[5], array1[5], array2[5], array3[5], array4[5];
+    int iterator = 1;
+    int* currLargest = array0;
 
-    buildArray(array0);
-    buildArray(array1);
-    buildArray(array2);
-    buildArray(array3);
-    buildArray(array4);
+    buildArray(iterator, array0);
+    iterator++;
 
-    for (std::size_t i = 0; i < 5; i++)
-    {
+    buildArray(iterator, array1);
+    currLargest = returnLarger(currLargest, array1);
+    iterator++;
 
-    }
+    buildArray(iterator, array2);
+    currLargest = returnLarger(currLargest, array2);
+    iterator++;
+
+    buildArray(iterator, array3);
+    currLargest = returnLarger(currLargest, array3);
+    iterator++;
+
+    buildArray(iterator, array4);
+    currLargest = returnLarger(currLargest, array4);
+
+    std::cout << currLargest[0] << " " << currLargest[5] << std::endl;
 
     return 0;
 }
 
-int buildArray(int array[])
+int buildArray(int inputIterator, int array[])
 {
-    for (std::size_t i = 0; i < 4; i++) // Populating array
+    array[0] = inputIterator;
+
+    for (std::size_t i = 1; i < 5; i++) // Populating array
     {
         std::cin >> array[i];
 
-        if (i = 0)
+        if (i == 1)
         {
-            array[4] = array[0];
+            array[5] = array[1];
             continue;
         }
         else
         {
-            array[4] = array[4] + array[i];
+            array[5] = array[5] + array[i];
         }
     }
+
+    return *array;
+}
+
+int* returnLarger(int* currLargest, int inputArray[])
+{
+    if (currLargest[5] < inputArray[5])
+    {
+        currLargest = inputArray;
+    }
+
+    return currLargest;
 }
