689f774d3c9acc275e994b5a876d18f89196cfa9
Muffinman <albusgithub@mevacy.com>
2024-07-12 15:55:56 -0600

    Numbers are reversed

--- 5. Kattis Problems CPP/11. Filip SSTREAM & INT TO STRING/main.cpp
+++ 5. Kattis Problems CPP/11. Filip SSTREAM & INT TO STRING/main.cpp
@@ -4,10 +4,15 @@
 
 int main(int argc, char *argv[])
 {
-    int inputNum1, inputNum2;
-    std::cin >> inputNum1 >> inputNum2;
+    std::string iN1, iN2;
+    std::cin >> iN1 >> iN2;
 
-    int revNum1, revNum2;
+    std::string rN1(iN1.rbegin(), iN1.rend()), rN2(iN2.rbegin(), iN2.rend());
+
+    std::cout << "Num1 Reversed: " << rN1 << ", Num2 Reversed: " << rN2 << std::endl;
+
     
     return 0;
 }
+
+// std::istringstream(*input*);
