01c8a0ec59d88d3e18318f844cfc72883e78196b
Muffinman <albusgithub@mevacy.com>
2024-07-12 11:03:09 -0600

    fully working i think!

--- 5. Kattis Problems in cpp/9. Ive Been Everywhere/main.cpp
+++ 5. Kattis Problems in cpp/9. Ive Been Everywhere/main.cpp
@@ -1,47 +1,3 @@
-// https://open.kattis.com/problems/everywhere
-/*
-7/11/24 Kinda works, problems iterating resultArray. This is a good problem!
-
-TASK:
-    Alice travels a lot for her work. Each time she travels, she visits a single city before returning home.
-        Someone recently asked her “how many different cities have you visited for work?” Thankfully Alice has kept a
-        log of her trips. Help Alice figure out the number of cities she has visited at least once.
-
-INPUT:
-    The first line of input contains a single positive integer 'T'(<= 50) indicating the number of test cases. The
-        first line of each test case also contains a single positive integer 'n' indicating the number of work trips
-        Alice has taken so far. The following lines 'n' describe these trips. The 'i'th such line simply contains the
-        name of the city Alice visited on her 'i'th trip.
-    Alice’s work only sends her to cities with simple names: city names only contain lowercase letters, have at least
-        one letter, and do not contain spaces.
-    The number of trips is at most 100 and no city name contains more than 20 characters.
-
-    Example 1:
-2
-7
-saskatoon
-toronto
-winnipeg
-toronto
-vancouver
-saskatoon
-toronto
-3
-edmonton
-edmonton
-delta
-
-OUTPUT:
-    For each test case, simply output a single line containing a single integer that is the number of distinct cities
-        that Alice has visited on her work trips.
-    
-    Example 1:
-        4
-        2
-
-    Example 2:
-*/
-
 #include <iostream>
 #include <string>
 
@@ -93,8 +49,7 @@ int main(int argc, char *argv[])
 
     for (std::size_t p = 0; p < numTrips; p++)
     {
-        std::cout << resultArray[p] << " ";
-        // std::cout << resultArray[p] << std::endl;
+        std::cout << resultArray[p] << std::endl;
     }
 
     return 0;
