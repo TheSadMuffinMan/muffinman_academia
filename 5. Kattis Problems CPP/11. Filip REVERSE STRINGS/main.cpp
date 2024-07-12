eb8ffecb654a050d5fde2cc6a786b51d6bf7a131
Muffinman <albusgithub@mevacy.com>
2024-07-12 15:58:19 -0600

    Adding notation

--- 5. Kattis Problems CPP/11. Filip REVERSE STRINGS/main.cpp
+++ 5. Kattis Problems CPP/11. Filip REVERSE STRINGS/main.cpp
@@ -7,7 +7,9 @@ int main(int argc, char *argv[])
     std::string iN1, iN2;
     std::cin >> iN1 >> iN2;
 
-    std::string rN1(iN1.rbegin(), iN1.rend()), rN2(iN2.rbegin(), iN2.rend());
+    std::string rN1(iN1.rbegin(), iN1.rend()), rN2(iN2.rbegin(), iN2.rend()); // Reverse Iterators
+    // ^ Found @ https://www.geeksforgeeks.org/reverse-a-string-in-c-cpp-different-methods/
+    // Other methods (recursive, pointers, diff. libraries, )
 
     std::cout << "Num1 Reversed: " << rN1 << ", Num2 Reversed: " << rN2 << std::endl;
 
