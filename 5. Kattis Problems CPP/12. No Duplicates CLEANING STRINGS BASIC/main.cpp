021229c273ecdc6ffbe72b1e1696b09461444edb
Muffinman <albusgithub@mevacy.com>
2024-07-15 12:20:04 -0600

    Progress and success! Still working on.

--- 5. Kattis Problems CPP/12. No Duplicates CLEANING STRINGS BASIC/main.cpp
+++ 5. Kattis Problems CPP/12. No Duplicates CLEANING STRINGS BASIC/main.cpp
@@ -19,7 +19,29 @@ int main(int argc, char *argv[])
     for (std::size_t i = 0; i < maxSize; i++)
     {
         stringEndPos = inputString.find(' ', stringStartPos);
-        std::string indivWord = inputString.substr(stringStartPos, ());
+        std::string indivWord = inputString.substr(stringStartPos, (stringEndPos - stringStartPos));
+        
+        for (std::size_t j = 0; j < indivWord.size(); j++) // Sanitizing string.
+        {
+            indivWord.at(j) = toupper(indivWord.at(j));
+        }
+
+        wordArray[i] = indivWord;
+        stringStartPos = stringEndPos + 1;
+        // WORKING AS INTENDED TO THIS POINT.
+    }
+
+    for (std::size_t i = 0; i < maxSize; i++)
+    {
+        if (wordArray[i] == "x")
+        {
+            std::cout << "\nEND OF ARRAY." << std::endl;
+            break;
+        }
+        else
+        {
+            std::cout << wordArray[i] << std::endl;
+        }
     }
 
     return 0;
