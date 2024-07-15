98281ede809f49611a61519a68e3d182c681f5c7
Muffinman <albusgithub@mevacy.com>
2024-07-15 12:41:56 -0600

    wordArray is overpopulating..

--- 5. Kattis Problems CPP/12. No Duplicates CLEANING STRINGS BASIC/main.cpp
+++ 5. Kattis Problems CPP/12. No Duplicates CLEANING STRINGS BASIC/main.cpp
@@ -9,13 +9,14 @@ int main(int argc, char *argv[])
     getline(std::cin, inputString);
 
     std::string wordArray[maxSize];
-    for (std::size_t x = 0; x < maxSize; x++)
+    for (std::size_t x = 0; x < maxSize; x++) // Populating array.
     {
         wordArray[x] = "x";
     }
 
     std::size_t stringStartPos = 0;
     std::size_t stringEndPos = 0;
+    std::size_t numWords = 0;
     for (std::size_t i = 0; i < maxSize; i++)
     {
         stringEndPos = inputString.find(' ', stringStartPos);
@@ -27,18 +28,25 @@ int main(int argc, char *argv[])
         }
 
         wordArray[i] = indivWord;
-        stringStartPos = stringEndPos + 1;
+        numWords++;
+        stringStartPos = (stringEndPos + 1);
         // WORKING AS INTENDED TO THIS POINT.
+
+        if (stringStartPos > inputString.size())
+        {
+            break;
+        }
     }
+
+    std::cout << "***DEBUG*** numWords: " << numWords << std::endl;
+    std::cout << "***DEBUG*** inputString.size(): " << inputString.size() << std::endl;
+
+    std::cout << "\nEnd of program." << std::endl;
+    return 0;
 
     for (std::size_t i = 0; i < maxSize; i++)
     {
-        if (wordArray[i] == "x")
-        {
-            std::cout << "\nEND OF ARRAY." << std::endl;
-            break;
-        }
-        else
+        if (wordArray[i] != "x")
         {
             std::cout << wordArray[i] << std::endl;
         }
