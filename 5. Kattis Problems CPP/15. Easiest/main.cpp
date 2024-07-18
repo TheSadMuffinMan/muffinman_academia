a7cc76de78e51db3a23640c1ed4f17f1f4ae623f
Muffinman <albusgithub@mevacy.com>
2024-07-18 12:21:15 -0600

    Getting started on Easiest

--- 5. Kattis Problems CPP/15. Easiest/main.cpp
+++ 5. Kattis Problems CPP/15. Easiest/main.cpp
@@ -2,7 +2,16 @@
 
 int main(int argc, char *argv[])
 {
-    //
+    const int maxSize = 100;
+    int mainNum = 0;
+    std::cin >> mainNum;
+
+    int numArray[maxSize];
+    for (std::size_t x = 0; x < maxSize, x++;) // Populating array.
+    {
+        numArray[x] = 0;
+    }
+
     
     std::cout << "\nEnd of program." << std::endl;
     return 0;
