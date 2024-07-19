18a96ca056e28c162b8371f9a783a597da152ae7
Muffinman <albusgithub@mevacy.com>
2024-07-19 13:58:31 -0600

    Still working on decryption logic

--- 5. Kattis Problems CPP/16. Encoded Message 2D ARRAYS/main.cpp
+++ 5. Kattis Problems CPP/16. Encoded Message 2D ARRAYS/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <cmath>
 
+std::size_t twoDSize(std::string);
+void decryptString(std::string);
+
 int main(int argc, char *argv[])
 {
     std::size_t numLines = 0;
@@ -10,18 +13,66 @@ int main(int argc, char *argv[])
     for (std::size_t x = 0; x < numLines; x++) {inputStringArray[x] = "x";} // Populating inputStringArray.
 
     std::cin.ignore(1);
-    std::size_t totalChars = 0;
     for (std::size_t i = 0; i < numLines; i++)
     {
         getline(std::cin, inputStringArray[i]);
-        totalChars = (totalChars + inputStringArray[i].size());
     }
+    // WORKING AS INTENDED TO THIS POINT.
 
-    const std::size_t twoDArrayMaxLength = std::ceil(sqrt(totalChars));
-    std::cout << "***DEBUG*** totalChars: " << totalChars << std::endl;
-    std::cout << "***DEBUG*** 2dArrayMaxLength: " << twoDArrayMaxLength << std::endl;
+    decryptString(inputStringArray[0]);
 
 
     std::cout << "\nEnd of program." << std::endl;
     return 0;
+}
+
+// Function determines the appropriate size for a 2D array for the passed string.
+std::size_t twoDSize(std::string inputString)
+{
+    std::size_t returnNum = std::ceil(sqrt(inputString.size()));
+    return returnNum;
+}
+
+void decryptString(std::string inputString)
+{
+    // Determine array "square size".
+    // Split input string into equal substrings.
+    // Populate 2D array with each substring.
+    // Walk backwards through 2D array and print ea. char.
+
+    std::size_t twoDArraySize = twoDSize(inputString);
+
+    char charArray[twoDArraySize][twoDArraySize];
+    std::size_t arrayIndex = 0;
+    for (std::size_t x = 0; x < twoDArraySize; x++)
+    {
+        for (std::size_t y = 0; y < twoDArraySize; y++)
+        {
+            charArray[x][y] = inputString.at(arrayIndex);
+            arrayIndex++;
+        }
+    }
+    // FUNCTION WORKING AS INTENDED TO THIS POINT.
+
+    // DEBUGGING
+    for (std::size_t x = 0; x < twoDArraySize; x++)
+    {
+        for (std::size_t y = 0; y < twoDArraySize; y++)
+        {
+            std::cout << charArray[x][y] << " ";
+        }
+
+        std::cout << std::endl;
+    }
+
+    for (std::size_t x = twoDArraySize; x > 0; x--)
+    {
+        for (std::size_t y = 0; y < twoDArraySize; y++) // (std::size_t y = twoDArraySize; y > 0; y--)
+        {
+            std::cout << charArray[x][y] << " ";
+        }
+
+        std::cout << std::endl;
+    }
+
 }
