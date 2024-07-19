95ce546d80cba62130ac61338527300885ae391b
Muffinman <albusgithub@mevacy.com>
2024-07-19 16:01:28 -0600

    Calling it a day. Working on arithmatics

--- 5. Kattis Problems CPP/17. Mixed Fractions STREAMS/main.cpp
+++ 5. Kattis Problems CPP/17. Mixed Fractions STREAMS/main.cpp
@@ -16,8 +16,20 @@ int main(int argc, char *argv[])
 
         sstream << input;
         sstream >> num1 >> num2;
+        // WORKING AS INTENDED TO THIS POINT.
+        // std::cout << "Num1: " << num1 << "Num2: " << num2 << std::endl;
 
-        std::cout << "Num1: " << num1 << "Num2: " << num2 << std::endl;
+        inputVector.at(vectorIterator) = num1;
+        inputVector.at(vectorIterator + 1) = num2;
+        vectorIterator = (vectorIterator + 2);
+    }
+
+    while (!inputVector.empty())
+    {
+        int num1, num2;
+        num1 = inputVector.front();
+        num2 = inputVector.at(1);
+
     }
     
     std::cout << "\nEnd of program." << std::endl;
