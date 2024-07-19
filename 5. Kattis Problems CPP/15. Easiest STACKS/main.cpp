e0dea8338cdececd03fbb20099756d05bd8bce35
Muffinman <albusgithub@mevacy.com>
2024-07-19 10:27:56 -0600

    Fixing function functionality lol

--- 5. Kattis Problems CPP/15. Easiest STACKS/main.cpp
+++ 5. Kattis Problems CPP/15. Easiest STACKS/main.cpp
@@ -10,6 +10,7 @@ int main(int argc, char *argv[])
     int inputTestNum = 0;
     std::cin >> inputTestNum;
     const int targetNum = findSum(inputTestNum);
+    std::cout << "***DEBUG*** targetNum: " << targetNum << std::endl;
     
 
     int inputArray[maxSize];
@@ -31,9 +32,10 @@ int main(int argc, char *argv[])
     for (std::size_t i = 0; i < maxSize; i++)
     {
         int startNum = 10;
+
         while (true)
         {
-            int resultNum = findSum(startNum, inputTestNum);
+            int resultNum = findSum(startNum * inputTestNum);
 
             if (resultNum == targetNum)
             {
@@ -49,7 +51,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-// Function returns the sum of all digits inside of passed int.
+// WORKING. Function returns the sum of all digits inside of passed int.
 int findSum(int inputNum)
 {
     std::stack<int> workingStack;
@@ -74,37 +76,6 @@ int findSum(int inputNum)
 // Overloaded function that multiplies num1 and num2 to result in num3, then returns the sum of all the digits in num3.
 int findSum(int num1, int num2)
 {
-    std::stack<int> workingStack; // Stack to hold indiv digits.
-    int num3 = (num1 * num2);
-
-    while (num3 > 0)
-    {
-        int indivDigit = (num3 % 10); // This "returns" the last digit.
-        num3 /= 10; // "Removing" the last digit.
-        workingStack.push(indivDigit);
-    }
-
-    int returnSum = 0;
-    while (!workingStack.empty())
-    {
-        returnSum = (returnSum + workingStack.top());
-        workingStack.pop();
-    }
-
+    int returnSum = findSum(num1 * num2);
     return returnSum;
 }
-
-/*
-    int sumsArray[maxSize];
-    for (std::size_t x = 0; x < maxSize; x++) // Populating sumsArray.
-    {
-        sumsArray[x] = 0;
-    }
-
-    arrayIndex = 0;
-    while (inputArray[arrayIndex] != 0)
-    {
-        sumsArray[arrayIndex] = findSum(inputTestNum, inputArray[arrayIndex]);
-        arrayIndex++;
-    }
-*/
