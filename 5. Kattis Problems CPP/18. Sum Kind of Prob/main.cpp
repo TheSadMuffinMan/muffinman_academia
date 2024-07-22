4695a6572d0bed1fefb741a5721292387a479e69
Muffinman <albusgithub@mevacy.com>
2024-07-22 15:55:24 -0600

    Done for day, need to figure out cin.ignore next

--- 5. Kattis Problems CPP/18. Sum Kind of Prob/main.cpp
+++ 5. Kattis Problems CPP/18. Sum Kind of Prob/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
-#include <vector>
 
 int main(int argc, char *argv[])
 {
-    //
+    std::size_t numCases = 0;
+    std::cin >> numCases;
+    int currArray[numCases];
+    for (std::size_t x = 0; x < numCases; x++) {currArray[x] = 0;} // Populating array.
+
+    double factorialIterator = 0; // Need a double because factorials get large very quickly.
+    for (std::size_t i = 0; i < numCases; i++) // Populating currArray.
+    {
+        int currNum = 0;
+        std::cin.ignore(2); // Ignoring first two chars because we don't need them.
+        std::cin >> currNum;
+    }
+
+    for (std::size_t j = 0; j < numCases; j++)
+    {
+        std::cout << "***DEBUG*** currArray[" << j << "]: " << currArray[j] << std::endl;
+    }
     
     std::cout << "\nEnd of program." << std::endl;
     return 0;
