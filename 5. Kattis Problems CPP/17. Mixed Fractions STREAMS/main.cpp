30c91492222833ec9f5df5e2ff873ea7e034c9e9
Muffinman <albusgithub@mevacy.com>
2024-07-22 14:12:26 -0600

    Working on pulling data from vectors

--- 5. Kattis Problems CPP/17. Mixed Fractions STREAMS/main.cpp
+++ 5. Kattis Problems CPP/17. Mixed Fractions STREAMS/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <cmath>
 
 int main(int argc, char *argv[])
 {
@@ -23,13 +24,20 @@ int main(int argc, char *argv[])
         inputVector.at(vectorIterator + 1) = num2;
         vectorIterator = (vectorIterator + 2);
     }
+    std::size_t oldVectorIterator = vectorIterator;
 
-    while (!inputVector.empty())
+    vectorIterator = 0;
+    while (vectorIterator < oldVectorIterator)
     {
-        int num1, num2;
-        num1 = inputVector.front();
-        num2 = inputVector.at(1);
+        int inputNumerator, denominator;
+        inputNumerator = inputVector.at(vectorIterator);
+        denominator = inputVector.at(vectorIterator + 1);
 
+        int wholeNum = std::floor(inputNumerator / denominator);
+        int newNumerator = (inputNumerator % denominator);
+
+        std::cout << wholeNum << " " << newNumerator << " / " << denominator << std::endl;
+        vectorIterator = (vectorIterator + 2);
     }
     
     std::cout << "\nEnd of program." << std::endl;
