ea59f27882760fd50c1c6f3702b736e0988d240e
Muffinman <albusgithub@mevacy.com>
2024-07-26 14:21:06 -0600

    Array issue solved with junkNum lol

--- 5. Kattis Problems CPP/18. Sum Kind of Prob/main.cpp
+++ 5. Kattis Problems CPP/18. Sum Kind of Prob/main.cpp
@@ -5,14 +5,17 @@ int main(int argc, char *argv[])
     std::size_t numCases = 0;
     std::cin >> numCases;
     int currArray[numCases];
+    int junkNum = 0;
     for (std::size_t x = 0; x < numCases; x++) {currArray[x] = 0;} // Populating array.
 
     double factorialIterator = 0; // Need a double because factorials get large very quickly.
     for (std::size_t i = 0; i < numCases; i++) // Populating currArray.
     {
         int currNum = 0;
-        std::cin.ignore(2); // Ignoring first two chars because we don't need them.
+        std::cin >> junkNum;
         std::cin >> currNum;
+
+        currArray[i] = currNum;
     }
 
     for (std::size_t j = 0; j < numCases; j++)
