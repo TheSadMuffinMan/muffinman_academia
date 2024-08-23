c3c49c977809877e24b5bd6425b99e7a344163f5
Muffinman <albusgithub@mevacy.com>
2024-08-23 11:18:11 -0600

    Added ClearScreen function

---
+++ 0. General Knowledge/3. ClearScreen Function/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void clearScreen();
+
+// Function clears the screen
+void clearScreen()
+{
+    // use "cls" in windows and "clear" command in Mac and Linux
+    #ifdef _WIN32
+        system("clS");
+    #else
+        system("clear");
+    #endif
+}
