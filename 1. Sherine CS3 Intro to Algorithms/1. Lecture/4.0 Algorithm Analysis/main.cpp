bfddf9a3cda2e07a65be75a68ef70972eab212d0
Muffinman <albusgithub@mevacy.com>
2024-08-23 11:13:13 -0600

    End of lecture

---
+++ 1. Sherine CS3 Intro to Algorithms/1. Lecture/4.0 Algorithm Analysis/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+As a programmer, you will need to "wear" plenty of hats.
+    For example, a programmer/student may need to...
+        - Develop a working solution.
+        - Solve a problem efficiently for a client.
+        - Understand a problem for a theoretician (using the scientific method).
+
+Running Time Analysis:
+    We care because making this analysis allows us to make more efficient and better decisions in general.
+    Examples include...
+        The Discrete Fourier transform - break down waveform of N samples into periodic components.
+        N-body simulation - Simulates gravitational interactions among N bodies.
+
+THE CHALLENGE/QUESTION:
+    Will my program be able to solve a large practical input?
+        Yes, we will use the scientific method to understand performance.
+*/
+
+int main(int argc, char *argv[])
+{
+    
+    return 0;
+}
