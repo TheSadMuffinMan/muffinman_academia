89384822f97aa251059fbf21e5aa2bdc35d7570e
Muffinman <albusgithub@mevacy.com>
2024-08-26 09:48:52 -0600

    Working on Theory Assignment

--- 1. Sherine CS3 Intro to Algorithms/2. Group Work/Theory Task 1/main.cpp
+++ 1. Sherine CS3 Intro to Algorithms/2. Group Work/Theory Task 1/main.cpp
@@ -6,11 +6,11 @@ using namespace std;
 
 /*
 QUESTION 1: (Question 1.15)
-Given a sequence of input pairs that causes the weighted Q-U algorithm with path compression by halving
+Give a sequence of input pairs that causes the weighted Q-U algorithm with path compression by halving
     (Program 1.4) to produce a path of length 4.
 
 QUESTION 2: (Question 1.22)
-Modify the program to generate random pairs of integers between 0 and (N-1) instead of ready them from standard input,
+Modify the program to generate random pairs of integers between 0 and (N-1) instead of reading them from standard input,
     and to loop until N-1 union operations have been formed. Run your program for for N = (10^3, 10^4, 10^5, 10^6) and
     print out the total number of edges generated for each value of N.
 
