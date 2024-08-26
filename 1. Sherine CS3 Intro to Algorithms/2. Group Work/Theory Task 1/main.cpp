e8e57fd22f9a3d06bac49c31ba33c6e2debfdb6e
Muffinman <albusgithub@mevacy.com>
2024-08-26 08:03:49 -0600

    Start of lecture and updating HW

--- 1. Sherine CS3 Intro to Algorithms/2. Group Work/Theory Task 1/main.cpp
+++ 1. Sherine CS3 Intro to Algorithms/2. Group Work/Theory Task 1/main.cpp
@@ -4,7 +4,10 @@
 using namespace std;
 
 /*
-STUFF
+QUESTION 1: Given a sequence of input pairs that causes the weighted Q-U algorithm with path compression by halving
+    (Program 1.4) to produce a path of length 4.
+
+
 */
 int main(int argc, char *argv[])
 {
