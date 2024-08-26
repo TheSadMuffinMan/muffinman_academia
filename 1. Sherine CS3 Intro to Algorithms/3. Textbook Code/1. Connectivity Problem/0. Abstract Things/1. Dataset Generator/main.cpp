e9ea29411d3c238be2c168b0905e89e3d0ed8a36
Muffinman <albusgithub@mevacy.com>
2024-08-26 15:41:02 -0600

    Working random data set generator

--- 1. Sherine CS3 Intro to Algorithms/3. Textbook Code/1. Connectivity Problem/0. Abstract Things/1. Dataset Generator/main.cpp
+++ 1. Sherine CS3 Intro to Algorithms/3. Textbook Code/1. Connectivity Problem/0. Abstract Things/1. Dataset Generator/main.cpp
@@ -5,25 +5,43 @@
 using namespace std;
 
 /*
-Program must...
-    Generate a data set inside of a .txt document.
-
+Program generates a data set consisting of two ints (0 < num < 9) of "dataSetSize".
 */
 
 int main(int argc, char *argv[])
 {
     std::string dumpVar = " ";
-    std::cout << "/nDo you have a blank output.txt document inside this directory? If so, continue." << std::endl;
+    std::cout << "\nDo you have a blank output.txt document inside this directory? If so, continue." << std::endl;
     std::getline(std::cin, dumpVar);
 
     // std::ifstream inputStream;
     std::ofstream outputStream;
-    outputStream.open("output.txt");
-    
+    outputStream.open("output.txt"); // Opening output.txt
+    // outputStream.write("OUTPUT FILE");
 
-    int dataSetSize = 0;
-    std::cout << "How large would you like the data set to be?(int): ";
+    if (!outputStream.is_open()) // Error catching: if the file fails to open, program will not proceed.
+    {
+        std::cout << "\noutput.txt failed to open, program terminating." << std::endl;
+        outputStream.close();
+        return 0;
+    }    
+
+    std::size_t dataSetSize = 0;
+    std::cout << "How large would you like the data set to be?(size_t): ";
     std::cin >> dataSetSize;
+
+    std::cout << "\nPress any key to start data set generation." << std::endl;
+    std::cin.ignore(1);
+    std::getline(std::cin, dumpVar);
+
+    int num1, num2;
+    for (std::size_t i = 0; i < dataSetSize; i++)
+    {
+        num1 = (rand() % 9);
+        num2 = (rand() % 9);
+
+        outputStream << num1 << " " << num2 << std::endl;
+    }
 
     outputStream.close();
     std::cout << "\nEnd of program." << std::endl;
