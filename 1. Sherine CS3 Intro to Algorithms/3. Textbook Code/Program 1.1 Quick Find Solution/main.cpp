f199c1f0378a4407ed53c49ed6271ce62cbc7a4a
Muffinman <albusgithub@mevacy.com>
2024-08-29 14:22:19 -0600

    Small updates

--- 1. Sherine CS3 Intro to Algorithms/3. Textbook Code/Program 1.1 Quick Find Solution/main.cpp
+++ 1. Sherine CS3 Intro to Algorithms/3. Textbook Code/Program 1.1 Quick Find Solution/main.cpp
@@ -1,6 +1,14 @@
-#include <iostream>
+/* Program 1.1 (in textbook)
+This program reads a sequence of pairs of nonnegative integers less than N from standard input (interpreting
+    the pair p q to mean “connect object p to object q”) and prints out pairs representing objects that are not
+    yet connected. It maintains an array id that has an entry for each object, with the property that id[p] and
+    id[q] are equal if and only if p and q are connected. For simplicity, we define N as a compile-time constant.
+    Alternatively, we could take it from the input and allocate the id array dynamically.
+*/
 
-using namespace std;
+#include <iostream>
+#include <stdio.h>
+#define N 10000
 
 int main(int argc, char *argv[])
 {
