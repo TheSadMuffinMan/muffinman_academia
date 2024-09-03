96f75d5678122da63fe4da1f05f5bc80bb1f2483
Muffinman <albusgithub@mevacy.com>
2024-09-03 11:16:48 -0600

    Set up of basic Python knowledge folder

---
+++ 0. General Knowledge/5. Python Basics/superBasic.py
@@ -0,0 +1,9 @@
+# To compile and run, navigate to file repo and input "python 3 *filename*.py".
+# <-- Pound key is for comments.
+# "\" is an escape key.
+
+print("Program start.\n")
+
+num1 = int(input("Please input an int to continue: ")) # Variable declaration and user input.
+
+print("You input: ", num1)
