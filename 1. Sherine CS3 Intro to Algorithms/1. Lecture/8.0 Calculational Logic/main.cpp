0686492762218a6e94801b6f428b94d2570d834c
Muffinman <albusgithub@mevacy.com>
2024-09-06 08:09:19 -0600

    Knights and Knaves fun

--- 1. Sherine CS3 Intro to Algorithms/1. Lecture/8.0 Calculational Logic/main.cpp
+++ 1. Sherine CS3 Intro to Algorithms/1. Lecture/8.0 Calculational Logic/main.cpp
@@ -3,16 +3,26 @@ KNIGHTS vs KNAVES PROBLEM
 You are on an island full of natives that are composed of two groups: knights and knaves.
     Knights always tell the truth, and knaves always lie.
 
-PROBLEMS...
-stuff
+P1) You come across a native. They tell you "There is gold on the island" and "I am a knight". Are they
+    telling the truth?
+        ANSWER: We cannot definitely answer whether there is gold on the island.
+        LOGIC: (A == answer), (G == presence of gold).
+            A = (A = G);
+            (A = A) = G;
+            true = G;
+        TAKEAWAY: Some questions do not have an answer.
+
+P2) You come across two natives.
 */
 
 /*
 CALCULATIONAL LOGIC
 aka Boolean Logic.
 Has many properties:
-    Reflexive: p = p.
-    Symmetric: (p = q) = (q = p)
+    REFLEXIVE: p = p.
+    SYMMETRIC: (p = q) = (q = p)
+    TRANSITIVE: if (p = r) and (q = r), then (p = r).
+    ASSOCIATIVE: (p = (q = r)) = ((p = q) = r).
 */
 
 #include <iostream>
