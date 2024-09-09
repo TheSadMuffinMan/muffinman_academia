038f8afc44462ad464cbad4a9e03fd7ecbbc01cc
Muffinman <albusgithub@mevacy.com>
2024-09-09 08:52:24 -0600

    Lecture complete

--- 1. Sherine CS3 Intro to Algorithms/1. Lecture/9.0 Recursion/main.cpp
+++ 1. Sherine CS3 Intro to Algorithms/1. Lecture/9.0 Recursion/main.cpp
@@ -19,14 +19,39 @@ Many computational artifacts are naturally self-referential.
 One unfortunate aspect of Recursion is that to prove functionality, you often need to provide mathmatical proofs.
 
 Recursion IS very good for algorithmic trees.
-    
+*/
+
+/* Towers of Hanoi fun.
+    Has the same exact solution as the ruler problem (below).
 */
 
 #include <iostream>
+#include <string>
+
+std::string hanoi(int n, bool left)
+{
+    if (n == 0) {return " ";}
+
+    std::string move;
+    if (left)
+    {
+        move = std::to_string(n) + "L";
+    } else {
+        move = std::to_string(n) + "R";
+    }
+
+    return hanoi(n-1, !left) + move + hanoi(n-1, !left);
+}
 
 int main(int argc, char *argv[])
 {
     std::cout << "\nProgram start." << std::endl;
+
+    int n = 0;
+    std::cout << "How many blocks would you like?: ";
+    std::cin >> n;
+
+    std::cout << hanoi(n, false) << std::endl;
     
     std::cout << "\nEnd of program." << std::endl;
     return 0;
