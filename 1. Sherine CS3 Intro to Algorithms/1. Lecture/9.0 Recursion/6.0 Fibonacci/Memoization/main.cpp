1064ef2368e24448d63d7663a24c9acce3563f93
Muffinman <albusgithub@mevacy.com>
2024-09-11 08:45:30 -0600

    Adding lecture notes and programs :D

---
+++ 1. Sherine CS3 Intro to Algorithms/1. Lecture/9.0 Recursion/6.0 Fibonacci/Memoization/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+const int MAX_N = 100;
+long long memo[MAX_N];
+
+long long F(int n) 
+{
+    if (n == 0) {return 0;}
+    if (n == 1) {return 1;}
+
+    if (memo[n] == 0)
+    {
+        memo[n] = F(n - 1) + F(n - 2);
+    }
+
+    return memo[n];
+}
+int main(int argc, char* argv[])
+{
+    int n = std::stoi(argv[1]);
+
+    // Initialize memoization array with 0
+    for (int i = 0; i < MAX_N; i++) {memo[i] = 0;}
+
+    std::cout << F(n) << std::endl;
+    return 0;
+}
--- 1. Sherine CS3 Intro to Algorithms/1. Lecture/9.0 Recursion/main.cpp
+++ 1. Sherine CS3 Intro to Algorithms/1. Lecture/9.0 Recursion/main.cpp
@@ -23,6 +23,7 @@ Many computational artifacts are naturally self-referential.
 One unfortunate aspect of Recursion is that to prove functionality, you often need to provide mathmatical proofs.
 
 Recursion IS very good for algorithmic trees.
+    It provides a hierarchical approach to trees, with benefits including increased efficiency.
 
 An example of recursion is an H tree program.
 */
