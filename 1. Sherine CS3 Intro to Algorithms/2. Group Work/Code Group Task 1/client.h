9fbcf05154f9af29ea7d06e238df379cf247656d
Muffinman <albusgithub@mevacy.com>
2024-09-12 11:48:39 -0600

    Setting up client class structure

--- 1. Sherine CS3 Intro to Algorithms/2. Group Work/Code Group Task 1/client.h
+++ 1. Sherine CS3 Intro to Algorithms/2. Group Work/Code Group Task 1/client.h
@@ -3,8 +3,23 @@
 class unionClient
 {
     public:
-        //
+        unionClient(); // Default Constructor, does not do much.
+
+        // Getters
+        int* getIDArray() {return _idArray;}
+        int* getSizeArray() {return _sizeArray;}
+
+        // Setters
+        void setIDArray(int* desiredGroup) {_idArray = desiredGroup;}
+        void setSizeArray(int* desiredArray) {_sizeArray = desiredArray;}
     
     private:
-        //
+        int* _idArray;
+        int* _sizeArray;
 };
+
+// Default constructor.
+unionClient::unionClient()
+{
+    //
+}
--- 1. Sherine CS3 Intro to Algorithms/2. Group Work/Code Group Task 1/main.cpp
+++ 1. Sherine CS3 Intro to Algorithms/2. Group Work/Code Group Task 1/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "client.h"
 
+// ***DEBUG*** CHANGE LATER FOR MORE DYNAMIC USE.
+#define N 1000 // Preprocessor command that "anchors" N = 1000.
+
 int main(int argc, char* argv[])
 {
     std::cout << "\nProgram start." << std::endl;
