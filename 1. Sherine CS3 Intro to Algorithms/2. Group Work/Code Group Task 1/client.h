229871ec7033278155991c035c30dac7784262e2
Muffinman <albusgithub@mevacy.com>
2024-09-12 11:58:41 -0600

    Starting union operation

--- 1. Sherine CS3 Intro to Algorithms/2. Group Work/Code Group Task 1/client.h
+++ 1. Sherine CS3 Intro to Algorithms/2. Group Work/Code Group Task 1/client.h
@@ -1,17 +1,20 @@
 #include <iostream>
 
+#define N 1000
+
 class unionClient
 {
     public:
-        unionClient(); // Default Constructor, does not do much.
+        unionClient(); // Default Constructor.
 
         // Getters
         int* getIDArray() {return _idArray;}
         int* getSizeArray() {return _sizeArray;}
-
         // Setters
         void setIDArray(int* desiredGroup) {_idArray = desiredGroup;}
         void setSizeArray(int* desiredArray) {_sizeArray = desiredArray;}
+
+        int* Union(int, int); // Union operation. Joins two clients.
     
     private:
         int* _idArray;
@@ -20,6 +23,24 @@ class unionClient
 
 // Default constructor.
 unionClient::unionClient()
+{
+    int tempIDArray[N], tempSizeArray[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        tempIDArray[i] = i;
+        // This sets every node to "point" to itself, meaning that all nodes are not connected to anything.
+
+        tempSizeArray[i] = i;
+        // This sets the size of every node to 1 (because there have been no union operations yet).
+    }
+
+    unionClient::setIDArray(tempIDArray);
+    unionClient::setSizeArray(tempSizeArray);
+}
+
+// Comment.
+int* unionClient::Union(int p, int q)
 {
     //
 }
