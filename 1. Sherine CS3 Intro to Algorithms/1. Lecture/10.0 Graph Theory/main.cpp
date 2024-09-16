7c8adbee6a0d0ec5145d81de440d5523b7bb4e7a
Muffinman <albusgithub@mevacy.com>
2024-09-16 08:12:36 -0600

    Lecture about graphs!

---
+++ 1. Sherine CS3 Intro to Algorithms/1. Lecture/10.0 Graph Theory/main.cpp
@@ -0,0 +1,28 @@
+/*
+If we allow multiple links to a node as well as from a node, the resulting data structure is called a GRAPH.
+
+A graph can be DIRECTED or UNDIRECTED.
+    DIRECTED: A graph in which each edge may only be traversed in a specific direction.
+    UNDIRECTED: Can navigate in any order.
+
+A graph can be be CONNECTED if you can get from any node to any other node by following edges.
+A Directed graph is strongly connected if you can get from any node to any other node by following
+    directed edges.
+
+A graph in which no paths exists from one node back to itself except by retracing one's steps is called
+    an ACYCLIC graph.
+
+A graph which can be drawn on a sheet of paper with no crossing edges is called a PLANAR graph.
+
+A graph in which all nodes are connected are called a COMPLETE graph.
+*/
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "\nProgram start." << std::endl;
+    
+    std::cout << "\nEnd of program." << std::endl;
+    return 0;
+}
