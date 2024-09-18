a7a02964c067a396d39a0d11eb2282899f8ef139
Muffinman <albusgithub@mevacy.com>
2024-09-18 08:08:11 -0600

    Undirected graphs lecture start

---
+++ 1. Sherine CS3 Intro to Algorithms/1. Lecture/10.0 Graph Theory/1. Undirected Graphs/main.cpp
@@ -0,0 +1,15 @@
+// WE HAVE ANOTHER 10 MINUTE QUIZ ON FRIDAY. GET HERE EARLY.
+/*
+UNDIRECTED GRAPH: consists of vertices connected pairwise by edges.
+    Vertices <==> nodes.
+
+*/
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "\nProgram start." << std::endl;
+    
+    std::cout << "\nEnd of program." << std::endl;
+    return 0;
+}
