76a449842612db9bc9f90c76286de5db4251db24
Muffinman <albusgithub@mevacy.com>
2024-09-20 08:22:21 -0600

    Lecture start

---
+++ 1. Sherine CS3 Intro to Algorithms/1. Lecture/10.0 Graph Theory/2. Depth First Search/main.cpp
@@ -0,0 +1,19 @@
+
+
+/*
+DEPTH FIRST SEARCH:
+    Is one of the oldest algorithms out there!
+    One of the best representations of a graph is a Maze, especially the Pac-Man Maze!
+    Goal: Systematically traverse a graph.
+    Idea: Mimic maze exploration // Function-call stack acts as a ball of string.
+*/
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "\nProgram start." << std::endl;
+    
+    std::cout << "\nEnd of program." << std::endl;
+    return 0;
+}
