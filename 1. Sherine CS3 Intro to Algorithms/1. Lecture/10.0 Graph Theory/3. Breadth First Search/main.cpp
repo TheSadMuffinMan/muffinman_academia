22089d946de39143ce2eb7540b1a1afea66fc5f8
Muffinman <albusgithub@mevacy.com>
2024-09-23 08:50:22 -0600

    Lecture BFS and Games complete

--- 1. Sherine CS3 Intro to Algorithms/1. Lecture/10.0 Graph Theory/3. Breadth First Search/main.cpp
+++ 1. Sherine CS3 Intro to Algorithms/1. Lecture/10.0 Graph Theory/3. Breadth First Search/main.cpp
@@ -1,13 +1,18 @@
 /*
 BREADTH FIRST SEARCH:
-
+- A graph traversal approach in which you start at a source node and layer by layer through the graph,
+    analyzing the nodes directly related to the source node.
+- Not a recursive data structure! :D
 */
 
 /* DATA STRUCTURE:
-
+- Utilizes a queue and places source node in the queue first.
+- Add to queue all unmarked vetices pointing form (source) and mark them.
 */
 
-/* PROPERTIES:
+/* STEPS:
+Remove vertex v from queue.
+Add to queue all unmarked vertices adjacent to v and mark them.
 */
 
 #include <iostream>
