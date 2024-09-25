f829addb8dd7a0724cbb4892f6b62e81fc70e48e
Muffinman <albusgithub@mevacy.com>
2024-09-25 09:48:19 -0600

    This library is pretty freakin cool

---
+++ 0. General Knowledge/5. Python Basics/autoGuiPaint.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+screenWidth, screenHeight = pyautogui.size() # Sets the screen width and height of primary monitor.
+print("Width: ", screenWidth)
+print("Height: ", screenHeight)
+
+# pyautogui.click()
--- 0. General Knowledge/5. Python Basics/pyAutoGuiBasics.py
+++ 0. General Knowledge/5. Python Basics/pyAutoGuiBasics.py
@@ -11,7 +11,13 @@ pyautogui.doubleClick() # Double clicks the mouse.
 pyautogui.click(currentMouseX, currentMouseY) # Clicks at X,Y.
 
 pyautogui.move(None, 10) # Moves the mouse 10 pixels down from it's current position.
+pyautogui.drag(None, -10) # Clicks and drags from relative position.
 
 pyautogui.write("Hello world!", interval = 0.25) # Types message with quarter second pause between each input.
-pyautogui.press('esc') # Simulates pressing the "ESC" key.
+# pyautogui.press('esc') # Simulates pressing the "ESC" key.
 
+# Need to install pyMsgBox library to use the following code...
+# pyautogui.alert("This is an alert box.")
+# pyautogui.confirm("Shall the program proceed?")
+# pyautogui.prompt("What is your name?")
+# pyautogui.password("Enter PW (text will be hidden): ")
