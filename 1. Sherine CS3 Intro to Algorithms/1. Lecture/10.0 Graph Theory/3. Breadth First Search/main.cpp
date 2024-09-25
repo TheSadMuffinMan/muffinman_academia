7fe45590bcbbba16c6057420068de8575c3ceedf
Muffinman <albusgithub@mevacy.com>
2024-09-25 08:28:54 -0600

    Makin our way through lecture BFS

--- 1. Sherine CS3 Intro to Algorithms/1. Lecture/10.0 Graph Theory/3. Breadth First Search/main.cpp
+++ 1. Sherine CS3 Intro to Algorithms/1. Lecture/10.0 Graph Theory/3. Breadth First Search/main.cpp
@@ -17,6 +17,18 @@ Add to queue all unmarked vertices adjacent to v and mark them.
 
 #include <iostream>
 
+class BreadthFirstPaths
+{
+    public:
+        //
+    private:
+        bool* marked; // Array to hold whether or not vertices have been visited.
+        int* edgeTo;
+        int* distTo;
+
+        // void bfs(Graph, int);
+};
+
 int main(int argc, char *argv[])
 {
     std::cout << "\nProgram start." << std::endl;
@@ -24,3 +36,28 @@ int main(int argc, char *argv[])
     std::cout << "\nEnd of program." << std::endl;
     return 0;
 }
+
+/* Breadth First Search Java implementation:
+public class BreadthFirstPaths
+{
+private boolean[] marked;
+private int[] edgeTo;
+private int[] distTo;
+...
+private void bfs(Graph G, int s) {
+Queue<Integer> q = new Queue<Integer>();
+q.enqueue(s);
+marked[s] = true;
+distTo[s] = 0;
+while (!q.isEmpty()) {
+int v = q.dequeue();
+for (int w : G.adj(v)) {
+if (!marked[w]) {
+q.enqueue(w);
+marked[w] = true;
+edgeTo[w] = v;
+distTo[w] = distTo[v] + 1;
+}
+}
+}
+*/
