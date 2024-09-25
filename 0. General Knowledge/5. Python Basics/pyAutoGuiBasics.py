2944b7b0316bec0c04602be2a6d7b8a14b309d54
Muffinman <albusgithub@mevacy.com>
2024-09-25 09:33:58 -0600

    Testing pyAutoGui

---
+++ 0. General Knowledge/5. Python Basics/pyAutoGuiBasics.py
@@ -0,0 +1,17 @@
+# Documentation located at:
+# https://github.com/asweigart/pyautogui/blob/master/README.md
+
+import pyautogui
+
+screenWidth, screenHeight = pyautogui.size() # Sets the screen width and height of primary monitor.
+currentMouseX, currentMouseY = pyautogui.position() # Returns current position of mouse.
+
+pyautogui.click() # Clicks the mouse at it's current location.
+pyautogui.doubleClick() # Double clicks the mouse.
+pyautogui.click(currentMouseX, currentMouseY) # Clicks at X,Y.
+
+pyautogui.move(None, 10) # Moves the mouse 10 pixels down from it's current position.
+
+pyautogui.write("Hello world!", interval = 0.25) # Types message with quarter second pause between each input.
+pyautogui.press('esc') # Simulates pressing the "ESC" key.
+
