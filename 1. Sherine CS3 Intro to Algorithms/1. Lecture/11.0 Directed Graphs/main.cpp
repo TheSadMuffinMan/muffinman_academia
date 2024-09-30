78420d0faed3088007e6bdad8d2655e57b41e59c
Muffinman <albusgithub@mevacy.com>
2024-09-30 08:49:14 -0600

    Digraphs lecture complete

--- 1. Sherine CS3 Intro to Algorithms/1. Lecture/11.0 Directed Graphs/main.cpp
+++ 1. Sherine CS3 Intro to Algorithms/1. Lecture/11.0 Directed Graphs/main.cpp
@@ -14,8 +14,9 @@ Digraph Problems:
     Strong Connectivity - Is there a directed path between all pairs of vertices?
     Transitive Closure - For which vertices v and w is there a path from v to w?
 
-How many different digraphs are there on V vertices?
-
+In practice, real-world digraphs tend to be sparse (meaning they have a huge number of vertices and a small
+    average vertex degree).
+Digraphs are best used for iterating over vertices pointing from vertex v.
 */
 
 #include <iostream>
