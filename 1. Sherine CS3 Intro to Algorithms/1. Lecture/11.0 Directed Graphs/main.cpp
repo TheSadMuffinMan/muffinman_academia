89887b61199cb1ce0fbcdd8ddb355166265c4682
Muffinman <albusgithub@mevacy.com>
2024-09-30 08:40:25 -0600

    Digraph API things

--- 1. Sherine CS3 Intro to Algorithms/1. Lecture/11.0 Directed Graphs/main.cpp
+++ 1. Sherine CS3 Intro to Algorithms/1. Lecture/11.0 Directed Graphs/main.cpp
@@ -1,3 +1,23 @@
+/*
+DIRECTED GRAPHS (Digraphs): A set of vertices connected pairwise by DIRECTED edges.
+    Directed Edges: A "one way street" type of connection.
+
+Outdegree: The amount of outbound connections each node vertice has.
+Indegree: The amound of inbound connections each vertice has.
+*/
+
+/*
+Digraph Problems:
+    Path - Is there a directed path from s to t?
+    Shortest Path - What is the shortest directed path from s to t?
+    Topological Sort - Can you draw a digraph so that all edges point upwards?
+    Strong Connectivity - Is there a directed path between all pairs of vertices?
+    Transitive Closure - For which vertices v and w is there a path from v to w?
+
+How many different digraphs are there on V vertices?
+
+*/
+
 #include <iostream>
 
 int main(int argc, char *argv[])
