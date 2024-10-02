7fd44de6315b75735b9eee89bb4062c4d0afb39d
Muffinman <albusgithub@mevacy.com>
2024-10-02 08:51:08 -0600

    Lecture complete

--- 1. Sherine CS3 Intro to Algorithms/1. Lecture/11.0 Directed Graphs/main.cpp
+++ 1. Sherine CS3 Intro to Algorithms/1. Lecture/11.0 Directed Graphs/main.cpp
@@ -14,6 +14,12 @@ Digraph Problems:
     Strong Connectivity - Is there a directed path between all pairs of vertices?
     Transitive Closure - For which vertices v and w is there a path from v to w?
 
+Digraph Applications:
+    Transportation: street intersection (vertex), one-way street (directed edge).
+    Web: web page (vertex), hyperlink (directed edge).
+    Food Web: species (vertex), predator-prey relationships (directed edge).
+    Scheduling: task (vertx), precedence constraint (directed edge).
+
 In practice, real-world digraphs tend to be sparse (meaning they have a huge number of vertices and a small
     average vertex degree).
 Digraphs are best used for iterating over vertices pointing from vertex v.
