c98372023d0da68c51f178a0c9ef4ff752f203d4
Muffinman <albusgithub@mevacy.com>
2024-10-10 12:58:39 -0600

    Small array notes

--- 0. General Knowledge/7. Cpp Arrays/main.cpp
+++ 0. General Knowledge/7. Cpp Arrays/main.cpp
@@ -2,13 +2,15 @@
 
 int main(int argc, char *argv[])
 {
+    // int array[] = int* array;
+
     std::cout << "\nProgram start." << std::endl;
     const int numElements = 1000;
     int* array = new int[numElements];
 
     /*
     Nifty way of determining array size.
-    This line is determining the total size of the array in bytes, and then dividing the total size by
+    This line determines the total size of the array in bytes, and then divides the total size by
         the size of an individual element, which will result in total number of elements (or "size").
     */
     int size = (sizeof(array) / sizeof(array[0]));
@@ -16,4 +18,5 @@ int main(int argc, char *argv[])
     delete[] array;
     std::cout << "\nEnd of program." << std::endl;
     return 0;
+
 }
