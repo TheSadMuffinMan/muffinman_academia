b8bb239f7eefffbde255dc3d04b0649906a5ab04
Muffinman <albusgithub@mevacy.com>
2024-10-10 15:32:57 -0600

    Group code set up

---
+++ 1. Sherine CS3 Intro to Algorithms/2. Group Work/Coding Group/2.0 Graph Theory Problem/buildFile.cpp
@@ -0,0 +1,13 @@
+/*
+This file is intended to help 
+*/
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "\nProgram start." << std::endl;
+    
+    std::cout << "\nEnd of program." << std::endl;
+    return 0;
+}
---
+++ 1. Sherine CS3 Intro to Algorithms/2. Group Work/Coding Group/2.0 Graph Theory Problem/main.cpp
@@ -0,0 +1,15 @@
+/*
+Program must...
+    - 
+*/
+#include <iostream>
+#include <fstream>
+#include <chrono>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "\nProgram start." << std::endl;
+    
+    std::cout << "\nEnd of program." << std::endl;
+    return 0;
+}
