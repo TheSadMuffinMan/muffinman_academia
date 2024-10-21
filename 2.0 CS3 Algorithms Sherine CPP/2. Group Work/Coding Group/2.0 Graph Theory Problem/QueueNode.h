648523166fd8318c1131a5dde525c2defb5d970a
Muffinman <albusgithub@mevacy.com>
2024-10-21 09:58:41 -0600

    Directed graph set up

---
+++ 2.0 CS3 Algorithms Sherine CPP/2. Group Work/Coding Group/2.0 Graph Theory Problem/QueueNode.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <iostream>
+
+template <class ItemType>
+class QueueNode
+{
+    public:
+        QueueNode();
+        
+        ItemType getData();
+        QueueNode<ItemType>* getNext();
+        QueueNode<ItemType>* getPrevious();
+
+        void setData(ItemType);
+        void setNext(QueueNode<ItemType>*);
+        void setPrevious(QueueNode<ItemType>*);
+
+    private:
+        ItemType _nodeData;
+        QueueNode<ItemType>* _previous;
+        QueueNode<ItemType>* _next;
+};
+
+// Constructor that initializes QueueNode's private members to NULL/nullptr.
+template <class ItemType>
+QueueNode<ItemType>::QueueNode()
+{
+    // ItemType _data;
+    _previous = nullptr;
+    _next = nullptr;
+}
+
+template <class ItemType>
+ItemType QueueNode<ItemType>::getData() {return _nodeData;}
+
+template <class ItemType>
+QueueNode<ItemType>* QueueNode<ItemType>::getNext() {return _next;}
+
+template <class ItemType>
+QueueNode<ItemType>* QueueNode<ItemType>::getPrevious() {return _previous;}
+
+template <class ItemType>
+void QueueNode<ItemType>::setData(ItemType passedData) {_nodeData = passedData;}
+
+template <class ItemType>
+void QueueNode<ItemType>::setNext(QueueNode* passedNode) {_next = passedNode;}
+
+template <class ItemType>
+void QueueNode<ItemType>::setPrevious(QueueNode* passedNode) {_previous = passedNode;}
--- 2.0 CS3 Algorithms Sherine CPP/2. Group Work/Coding Group/2.0 Graph Theory Problem/diGraph.h
+++ 2.0 CS3 Algorithms Sherine CPP/2. Group Work/Coding Group/2.0 Graph Theory Problem/diGraph.h
@@ -1,92 +1,14 @@
-/*
-Comments.
-*/
 #pragma once
-#include "node.h"
 #include <iostream>
-#include <fstream>
 #include <chrono>
+#include "MuffinQueue.h"
 
-template <class T>
-class DiGraph
+template <class ItemType>
+class DirectedGraph
 {
     public:
-        DiGraph()
-        {
-            int userNumNodes = 0;
-            std::cout << "How many Nodes would you like?: ";
-            std::cin >> userNumNodes;
-
-            setNumNodes(userNumNodes);
-            _nodeArray = Node<T>[getNumNodes()];
-
-            for (int i = 0; i < getNumNodes(); i++)
-            {
-                _nodeArray[i] = new Node<T>;
-                int userVertex[1];
-
-                std::cout << "\nVertex x: ";
-                std::cin >> userVertex[0];
-
-                std::cout << "\nVertex y: ";
-                std::cin >> userVertex[1];
-
-                _nodeArray[i]->setVertex(userVertex);
-            }
-
-            Node<T> displayNode = getIndivNode(0);
-
-            std::cout << "\n\nNumber of Nodes: " << getNumNodes() << std::endl;
-            std::cout << "Node 0: (";
-        }
-
-        ~DiGraph()
-        {
-            delete[] _nodeArray;
-            std::cout << "Memory cleaned up!" << std::endl;
-        }
-
-        // Getters
-        int getNumNodes() {return _numNodes;}
-        Node<T>* getNodeArray() {return _nodeArray;}
-        Node<T> getIndivNode(int requestedNode) {return _nodeArray[requestedNode];}
-
-        // Setters
-        void setNumNodes(int passedNumNodes) {_numNodes = passedNumNodes;}
-        void setNodeArray(Node<T>* passedArray) {_nodeArray = passedArray;}
-        void setIndivNode(int index, Node<T> passedNode) {_nodeArray[index] = passedNode;}
-
-        void buildGraphFromFile(std::string fileName) // ***FINISH LATER***
-        {
-            std::ifstream inputStream;
-            std::ofstream logStream;
-
-            logStream.open("log.txt");
-
-            // Checking to see if user included ".txt" in fileName.
-            int last4pos = (fileName.size() - 4);
-            std::string last4 = (fileName.substr(last4pos));
-            if (last4 != ".txt")
-            {
-                fileName = (fileName + ".txt");
-            }
-
-            std::cout << "\nOpening " << fileName << " ..." << std::endl;
-
-            inputStream.open(fileName);
-            if (!inputStream.is_open())
-            {
-                std::cerr << fileName << " failed to open, program terminating." << std::endl;
-                logStream << "Digraph failed to build from file." << std::endl;
-                
-                logStream.close();
-                return;
-            }
-
-            //
-        }
+        //
     
     private:
-        int _numNodes;
-        Node<T>* _nodeArray;
+        //
 };
