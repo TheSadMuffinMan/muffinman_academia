aa2053723f9c0743ac741be08392b0649ad86030
Muffinman <albusgithub@mevacy.com>
2024-10-24 12:43:56 -0600

    Working on Keegan implementation, reading file input

---
+++ 2.0 CS3 Algorithms Sherine CPP/2. Group Work/Coding Group/2.0 Graph Theory Problem/main.cpp
@@ -0,0 +1,48 @@
+/*
+Program is just the driver client for the Queue Class (MuffinQueue.h).
+*/
+
+#include <iostream>
+#include "digraph.h"
+
+int main(int argc, char *argv[])
+{
+    std::cout << "\nProgram start.\n" << std::endl;
+
+    DiGraph testGraph("inputFile.txt");
+
+    std::cout << "\nEnd of program." << std::endl;
+    return 0;
+}
+
+/* TESTING CODE
+
+    std::cout << "\nWeight 0 & 8 = 3?: " << std::boolalpha << (testGraph.getWeight(0, 8) == 3) << std::endl;
+    std::cout << "Weight 1 & 13 = 7?: " << std::boolalpha << (testGraph.getWeight(1, 3) == 7) << std::endl;
+    std::cout << "Weight 13 & 11 = 4?: " << std::boolalpha << (testGraph.getWeight(13, 11) == 4) << std::endl;
+    std::cout << "Weight 32 & 31 = 5?: " << std::boolalpha << (testGraph.getWeight(32, 31) == 5) << std::endl;
+
+    std::cout << "\nUsing DFS, shortest path between 1 & 3: ";
+    testGraph.depthFirstSearch(1, 3);
+    std::cout << std::endl;
+
+    std::cout << "\nBFS function for 1 & 3 returns: ";
+    testGraph.breadthFirstSearch(1, 3);
+    std::cout << std::endl;
+
+    std::cout << "\nBFS function for 5 & 7 returns: ";
+    testGraph.breadthFirstSearch(5, 7);
+    std::cout << std::endl;
+
+    std::cout << "\nBFS function for 20 & 26 returns: ";
+    testGraph.breadthFirstSearch(20, 26);
+    std::cout << std::endl;
+
+    DirectedGraph<int> testGraph(5);
+    testGraph.addVertex(0);
+    testGraph.addVertex(8);
+    testGraph.addEdge(0,8,3);
+
+    std::cout << "Vertex 0 exists: " << std::boolalpha << testGraph.vertexExists(0) << std::endl;
+
+*/
