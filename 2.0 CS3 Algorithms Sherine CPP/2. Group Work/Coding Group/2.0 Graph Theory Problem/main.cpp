fb2e3a7cd8ecfb1d07fe67d18b130768ec89ecbf
Muffinman <albusgithub@mevacy.com>
2024-10-24 20:12:15 -0600

    Still Seg Faulting :))))))))))))))))))))

--- 2.0 CS3 Algorithms Sherine CPP/2. Group Work/Coding Group/2.0 Graph Theory Problem/main.cpp
+++ 2.0 CS3 Algorithms Sherine CPP/2. Group Work/Coding Group/2.0 Graph Theory Problem/main.cpp
@@ -5,111 +5,92 @@ Program is just the driver client for the Queue Class (MuffinQueue.h).
 #include <iostream>
 #include "DirectedGraph.h"
 
+void mainShortestPath(DirectedGraph<int>&, int*);
+
 int main(int argc, char *argv[])
 {
-    int source1, source2, source3, destination1, destination2, destination3;
-    int path[50];
-    int pathWeight = 0;
-    std::string fileName;
+    int sourceArray[2];
+
+    std::string fileName = "inputFile.txt"; // ***DEBUG WHEN COMPLETE***
 
     std::cout << "\nProgram start.\n" << std::endl;
-    std::cin >> fileName;
+    // std::cin >> fileName; // ***DEBUG WHEN COMPLETE***
 
     DirectedGraph<int> workingGraph(fileName);
 
     std::cout << "\nProvide 3 sources (seperated by a space): ";
-    std::cin >> source1 >> source2 >> source3;
+
+    for (int i = 0; i < 3; i++)
+    {
+        std::cin >> sourceArray[i];
+    }
+
+    mainShortestPath(workingGraph, sourceArray);
+    mainShortestPath(workingGraph, sourceArray);
+    mainShortestPath(workingGraph, sourceArray);
+
+    std::cout << "\nEnd of program." << std::endl;
+    return 0;
+}
+
+void mainShortestPath(DirectedGraph<int>& currentGraph, int* sourceArray)
+{
+    int destination;
+    int path[50];
+    int pathWeight = 0;
 
     std::cout << "\n\tDestination 1?: ";
-    std::cin >> destination1;
-    std::cout << "Shortest Path 1 from vertex \"" << source1 << "\":" << std::endl;
-    if (workingGraph.shortestPath(source1, destination1, path, pathWeight))
+    std::cin >> destination;
+
+    std::cout << "Shortest Path 1 from vertex \"" << sourceArray[0] << "\":" << std::endl;
+    if (currentGraph.shortestPath(sourceArray[0], destination, path, pathWeight))
     {
         for (int i = 0; i < (pathWeight - 1); i++)
         {
             std::cout << path[i] << " --> ";
         }
-        std::cout << destination1 << std::endl;
+        std::cout << destination << std::endl;
     }
     else
     {
         std::cout << "Path not found." << std::endl;
     }
 
+    pathWeight = 0;
+    for (int i = 0; i < 50; i++)
+    {
+        path[i] = -1;
+    }
+    std::cout << "Shortest Path 2 from vertex \"" << sourceArray[1] << "\":" << std::endl;
+    if (currentGraph.shortestPath(sourceArray[1], destination, path, pathWeight))
+    {
+        for (int i = 0; i < (pathWeight - 1); i++)
+        {
+            std::cout << path[i] << " --> ";
+        }
+        std::cout << destination << std::endl;
+    }
+    else
+    {
+        std::cout << "Path not found." << std::endl;
+    }
 
-
-    std::cout << "\nEnd of program." << std::endl;
-    return 0;
+    pathWeight = 0;
+    for (int i = 0; i < 50; i++)
+    {
+        path[i] = -1;
+    }
+    std::cout << "Shortest Path 3 from vertex \"" << sourceArray[2] << "\":" << std::endl;
+    if (currentGraph.shortestPath(sourceArray[2], destination, path, pathWeight))
+    {
+        for (int i = 0; i < (pathWeight - 1); i++)
+        {
+            std::cout << path[i] << " --> ";
+        }
+        std::cout << destination << std::endl;
+    }
+    else
+    {
+        std::cout << "Path not found." << std::endl;
+    }
 }
-
-/* TESTING CODE
-
-    DirectedGraph<int> testGraph("inputFile.txt");
-
-    // std::cout << "\nCalling testGraph.gptShortestPath(5)..." << std::endl;
-    // testGraph.gptShortestPath(5);
-
-    int path[30]; // Assume a maximum path length of 30.
-    int pathLength;
-
-    std::cout << "\nCalling testGraph.shortestPath(21, 22,...):" << std::endl;
-    if (testGraph.shortestPath(21, 22, path, pathLength))
-    {
-        std::cout << "Shortest path from 21 to 22: ";
-        for (int i = 0; i < pathLength; ++i)
-        {
-            std::cout << path[i] << " --> ";
-        }
-        std::cout << std::endl;
-    }
-    else
-    {
-        std::cout << "No path found." << std::endl;
-    }
-
-    int path2[50];
-    int pathLength2;
-    std::cout << "\nCalling testGraph.shortestPath(20, 27,...):" << std::endl;
-    if (testGraph.shortestPath(20, 27, path2, pathLength2))
-    {
-        std::cout << "Shortest path from 20 to 27: ";
-        for (int i = 0; i < pathLength; ++i)
-        {
-            std::cout << path[i] << " --> ";
-        }
-        std::cout << std::endl;
-    }
-    else
-    {
-        std::cout << "No path found." << std::endl;
-    }
-
-    std::cout << "\nWeight 0 & 8 = 3?: " << std::boolalpha << (testGraph.getWeight(0, 8) == 3) << std::endl;
-    std::cout << "Weight 1 & 13 = 7?: " << std::boolalpha << (testGraph.getWeight(1, 3) == 7) << std::endl;
-    std::cout << "Weight 13 & 11 = 4?: " << std::boolalpha << (testGraph.getWeight(13, 11) == 4) << std::endl;
-    std::cout << "Weight 32 & 31 = 5?: " << std::boolalpha << (testGraph.getWeight(32, 31) == 5) << std::endl;
-
-    std::cout << "\nUsing DFS, shortest path between 1 & 3: ";
-    testGraph.depthFirstSearch(1, 3);
-    std::cout << std::endl;
-
-    std::cout << "\nBFS function for 1 & 3 returns: ";
-    testGraph.breadthFirstSearch(1, 3);
-    std::cout << std::endl;
-
-    std::cout << "\nBFS function for 5 & 7 returns: ";
-    testGraph.breadthFirstSearch(5, 7);
-    std::cout << std::endl;
-
-    std::cout << "\nBFS function for 20 & 26 returns: ";
-    testGraph.breadthFirstSearch(20, 26);
-    std::cout << std::endl;
-
-    DirectedGraph<int> testGraph(5);
-    testGraph.addVertex(0);
-    testGraph.addVertex(8);
-    testGraph.addEdge(0,8,3);
-
-    std::cout << "Vertex 0 exists: " << std::boolalpha << testGraph.vertexExists(0) << std::endl;
-
-*/
