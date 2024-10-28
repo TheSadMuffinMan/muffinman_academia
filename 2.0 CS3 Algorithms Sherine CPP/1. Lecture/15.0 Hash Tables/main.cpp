6d9b4f95b186915199c2ef055a4fc7b63cd67deb
Muffinman <albusgithub@mevacy.com>
2024-10-28 08:09:59 -0600

    Start lecture hash tables

---
+++ 2.0 CS3 Algorithms Sherine CPP/1. Lecture/15.0 Hash Tables/main.cpp
@@ -0,0 +1,17 @@
+/* HASH TABLES:
+Hash Tables' goals are to increase time efficiency greater than our standard logarithmic time.
+
+Hashing basic plan:
+HASH FUNCTION: Method for computing array index from key.
+
+*/
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "\nProgram start." << std::endl;
+    
+    std::cout << "\nEnd of program." << std::endl;
+    return 0;
+}
