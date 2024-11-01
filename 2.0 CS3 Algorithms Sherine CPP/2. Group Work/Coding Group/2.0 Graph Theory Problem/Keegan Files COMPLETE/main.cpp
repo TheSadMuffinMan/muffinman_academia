33234373a4c13d473d05be007a1f16bf874a1043
Muffinman <albusgithub@mevacy.com>
2024-11-01 13:50:58 -0600

    Finished logging

--- 2.0 CS3 Algorithms Sherine CPP/2. Group Work/Coding Group/2.0 Graph Theory Problem/Keegan Files COMPLETE/main.cpp
+++ 2.0 CS3 Algorithms Sherine CPP/2. Group Work/Coding Group/2.0 Graph Theory Problem/Keegan Files COMPLETE/main.cpp
@@ -18,6 +18,11 @@ fourth and on = source vertex, destination vertex, edge weight
 
 using namespace std;
 
+typedef std::chrono::steady_clock Time; // Makes is to where we don't have to type entire library every time.
+
+void logEvent(std::string);
+std::string buildString(int, int, std::chrono::_V2::high_resolution_clock::duration);
+
 int main(int argc, char* argv[]){
 	if(argc<2){
 		std::cerr<<"Error: Missing filename as main function arg.\n";
@@ -64,27 +69,10 @@ int main(int argc, char* argv[]){
 		dg.addEdge(vFrom, vTo, edgeWeight); // add connections in graph
 	}
 	file.close();
+    std::string printString = "Graph with " + Gsize;
+    printString = printString + " vertices created.";
+    logEvent(printString);
 
-    std::ofstream logStream;
-    logStream.open("log.txt", std::ios::app);
-    if (!logStream.is_open())
-    {
-        std::cerr << "Log stream failed to open, aborting." << std::endl;
-        return 0;
-    }
-
-    // Getting the current time from Chrono Library.
-    auto now = std::chrono::system_clock::now();
-
-    // Casting "now" to time_t type.
-    std::time_t now_time_t = std::chrono::system_clock::to_time_t(now);
-
-    // Converting "now_time_t" to a tm struct.
-    std::tm now_tm = *std::localtime(&now_time_t);
-
-    logStream << "[" << std::put_time(&now_tm, "%Y-%m-%d %H:%M:%S") << "]";
-    logStream << " Graph with " << Gsize << " nodes created." << std::endl;
-    logStream.close();
 
 	int input, v1,v2,v3, destV;
 	cout<<"Enter a negative integer to close the program.\n";
@@ -102,37 +90,124 @@ int main(int argc, char* argv[]){
 		cin>>destV;
 		if(destV<0) break;
 		
-		cout<<"Shortest path 1 from "<<v1<<": ";
-		dg.dijkstraSPT(v1, destV);
-		cout<<"Shortest path 2 from "<<v2<<": ";
-		dg.dijkstraSPT(v2, destV);
-		cout<<"Shortest path 3 from "<<v3<<": ";
-		dg.dijkstraSPT(v3, destV);
+		cout<<"Shortest path 1 from "<<v1<<": ";   
 
+        auto timeStart = Time::now();
+		dg.dijkstraSPT(v1, destV);
+        auto timeStop = Time::now();
+        auto duration = Time::duration(timeStop - timeStart);
+        printString = buildString(v1, destV, duration);
+        logEvent(printString);
+
+		cout<<"Shortest path 2 from "<<v2<<": ";
+        timeStart = Time::now();
+		dg.dijkstraSPT(v2, destV);
+        timeStop = Time::now();
+        duration = Time::duration(timeStop - timeStart);
+        printString = buildString(v2, destV, duration);
+        logEvent(printString);
+
+		cout<<"Shortest path 3 from "<<v3<<": ";
+        timeStart = Time::now();
+		dg.dijkstraSPT(v3, destV);
+        auto duration = Time::duration(timeStop - timeStart);
+        printString = buildString(v3, destV, duration);
+        logEvent(printString);
 
 		
 		cout<<"\nDestination 2?: ";
 		cin>>destV;
 		if(destV<0) break;
 		
-		cout<<"Shortest path 1 from "<<v1<<": ";
+        auto timeStart = Time::now();
 		dg.dijkstraSPT(v1, destV);
+        auto timeStop = Time::now();
+        auto duration = Time::duration(timeStop - timeStart);
+        printString = buildString(v1, destV, duration);
+        logEvent(printString);
+
 		cout<<"Shortest path 2 from "<<v2<<": ";
+        timeStart = Time::now();
 		dg.dijkstraSPT(v2, destV);
+        timeStop = Time::now();
+        duration = Time::duration(timeStop - timeStart);
+        printString = buildString(v2, destV, duration);
+        logEvent(printString);
+
 		cout<<"Shortest path 3 from "<<v3<<": ";
+        timeStart = Time::now();
 		dg.dijkstraSPT(v3, destV);
+        auto duration = Time::duration(timeStop - timeStart);
+        printString = buildString(v3, destV, duration);
+        logEvent(printString);
 
 		cout<<"\nDestination 3?: ";
 		cin>>destV;
 		if(destV<0) break;
 		
-		cout<<"Shortest path 1 from "<<v1<<": ";
+        auto timeStart = Time::now();
 		dg.dijkstraSPT(v1, destV);
+        auto timeStop = Time::now();
+        auto duration = Time::duration(timeStop - timeStart);
+        printString = buildString(v1, destV, duration);
+        logEvent(printString);
+
 		cout<<"Shortest path 2 from "<<v2<<": ";
+        timeStart = Time::now();
 		dg.dijkstraSPT(v2, destV);
+        timeStop = Time::now();
+        duration = Time::duration(timeStop - timeStart);
+        printString = buildString(v2, destV, duration);
+        logEvent(printString);
+
 		cout<<"Shortest path 3 from "<<v3<<": ";
+        timeStart = Time::now();
 		dg.dijkstraSPT(v3, destV);
+        auto duration = Time::duration(timeStop - timeStart);
+        printString = buildString(v3, destV, duration);
+        logEvent(printString);
 	}
 
 	return 0;
+}
+
+// Function logs the current Date/Time and prints passed string afterwards.
+void logEvent(std::string passedString)
+{
+    std::ofstream logStream;
+    logStream.open("log.txt", std::ios::app);
+    if (!logStream.is_open())
+    {
+        std::cerr << "Log stream failed to open, aborting." << std::endl;
+        return;
+    }
+
+    // Getting the current time from Chrono Library.
+    auto now = std::chrono::system_clock::now();
+
+    // Casting "now" to time_t type.
+    std::time_t now_time_t = std::chrono::system_clock::to_time_t(now);
+
+    // Converting "now_time_t" to a tm struct.
+    std::tm now_tm = *std::localtime(&now_time_t);
+
+    logStream << "[" << std::put_time(&now_tm, "%Y-%m-%d %H:%M:%S") << "]";
+    logStream << " " << passedString;
+    logStream.close();
+}
+
+// Function builds the log file string.
+std::string buildString(int sourceV, int destV, std::chrono::_V2::high_resolution_clock::duration duration)
+{
+    std::string returnString = "BFS between vertex " + sourceV;
+    std::string tempString = " and vertex " + destV;
+    returnString = returnString + tempString;
+
+    tempString = " performed in " + duration.count();
+    returnString = returnString + tempString;
+
+    tempString = " ns.";
+    returnString = returnString + tempString;
+
+    return returnString;
 }
