007701e32378cd79a2cb992ef873a49acc7bc710
Muffinman <albusgithub@mevacy.com>
2024-11-06 08:51:05 -0700

    Computational Theory and Regular Expression lecture

---
+++ 2.0 CS3 Algorithms Sherine CPP/1. Lecture/16.0 Computational Theory/main.cpp
@@ -0,0 +1,63 @@
+/* COMPUTATIONAL THEORY:
+
+Fundamental Questions:
+    - What can a computer do?
+    - What can a computer do with limited resources?
+
+General Approach:
+    - Don't talk about specific machines or problems.
+    - Consider minimal abstract machines.
+    - Consider general classes of problems.
+
+Key Terms:
+    - Abstract machine: defined by specific rules for transforming input to output.
+    - Deterministic Finite Automata (DFAs): a computer.
+    - Regular Expression (RE): the basic system used to quantify a string.
+
+Use Cases:
+    - Pattern matching (Ex: amino acid searching, genomics, scrubbing valid email address input, etc).
+*/
+
+/* REGULAR EXPRESSION (RE):
+===============================================================================
+    OPERATION     | EXAMPLE RE  | Matches (in the set) |    Does NOT match    |
+===============================================================================
+   concatenation  |   aabaab    |       aabaab         | *every other string* |
+===============================================================================
+     wildcard     |  .u.u.u.    |   cumulus, jugulum   | succubus, tumultuous |
+===============================================================================
+      union       |  aa | baab  |      aa, baab        | *every other string* |
+===============================================================================
+     closure      |    ab*a     |      aa, abbba       |     ab, ababa        |
+===============================================================================
+    parenthesis   |  a(a|b)aab  |    aaaab, abaab      | *every other string* |
+    parenthesis   |   (ab)*a    |    a, ababababa      |     aa, abbba        |
+===============================================================================
+
+
+    Examples:
+===========================================================================
+ Regular Expression (RE)  |        Matches        |    Does not match     |
+===========================================================================
+        .*spb.*           | raspberry, crispbread |  subspace, subspecies |
+===========================================================================
+   a* | (a*ba*ba*ba*)*    | bbb, aaa, bbbaababbaa |    b, bb, baabbaa     |
+===========================================================================
+       .*0....            |   1000234, 98701234   | 111111111, 403982772  |
+===========================================================================
+                          |     ...gcgctg...      |         gcgcgg        |
+   .*gcg(cgg|agg)*ctg.*   |    ...gcgcggctg...    |      cggcggcggctg     |
+                          |   ...gcgcggaggctg...  |       gcgcaggctg      |
+===========================================================================
+
+*/
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "\nProgram start." << std::endl;
+    
+    std::cout << "\nEnd of program." << std::endl;
+    return 0;
+}
