76cece645fee990fe1bb8665d9742dc905ec425b
Muffinman <albusgithub@mevacy.com>
2024-11-11 08:23:01 -0700

    Notes update

--- 2.0 CS3 Algorithms Sherine CPP/1. Lecture/17.0 Balanced Search Trees/main.cpp
+++ 2.0 CS3 Algorithms Sherine CPP/1. Lecture/17.0 Balanced Search Trees/main.cpp
@@ -1,6 +1,12 @@
-/* 2-3 TREE
+/* 2-3 TREES
     • One major property of a 2-3 tree is that the algorithm automatically balances the tree.
-    • 
+    • Balance is guarenteed by following this recursive order of operations:
+        - Compare search item against node.
+            - If greater than node, move right.
+            - If in between two values inside node, traverse middle path.
+            - If less than node, move left.
+    • We NEVER move nodes "down" the tree: the nodes will always be split and moved up the tree.
+    • This application is used when memory is limited.
 
 */
 
