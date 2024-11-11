c51afcd4b42d7c9f53077c6c62816e1c199b7176
Muffinman <albusgithub@mevacy.com>
2024-11-11 14:29:35 -0700

    Cleaning up classes

--- 2.0 CS3 Algorithms Sherine CPP/2. Group Work/Theory Group/Theory Task 2/hashChaining.h
+++ 2.0 CS3 Algorithms Sherine CPP/2. Group Work/Theory Group/Theory Task 2/hashChaining.h
@@ -10,36 +10,40 @@ class HashChaining
     public:
         HashChaining() : _table(TABLE_SIZE) {}
 
-        void insert(int key)
-        {
-            int index = hashFunction(key);
-            _table[index].push_back(key);
-        }
-
-        bool search(int key)
-        {
-            int index = hashFunction(key);
-            for (int element : _table[index])
-            {
-                if (element == key) return true;
-            }
-            return false;
-        }
-
-        void display()
-        {
-            std::cout << "Hash Table with Chaining:" << std::endl;
-            for (int i = 0; i < TABLE_SIZE; i++)
-            {
-                std::cout << i << ": ";
-                for (int element : _table[i])
-                {
-                    std::cout << element << " -> ";
-                }
-                std::cout << "NULL" << std::endl;
-            }
-        }
+        void insert(int);
+        bool search(int);
+        void display();
 
     private:
         std::vector<std::list<int>> _table;
 };
+
+void HashChaining::insert(int key)
+{
+    int index = hashFunction(key);
+    _table[index].push_back(key);
+}
+
+bool HashChaining::search(int key)
+{
+    int index = hashFunction(key);
+    for (int element : _table[index])
+    {
+        if (element == key) return true;
+    }
+    return false;
+}
+
+void HashChaining::display()
+{
+    std::cout << "Hash Table with Chaining:" << std::endl;
+    for (int i = 0; i < TABLE_SIZE; i++)
+    {
+        std::cout << i << ": ";
+        for (int element : _table[i])
+        {
+            std::cout << element << " -> ";
+        }
+        std::cout << "NULL" << std::endl;
+    }
+}
