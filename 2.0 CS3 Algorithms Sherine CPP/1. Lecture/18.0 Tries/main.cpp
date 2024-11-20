668cbacad6bfd17288cda13243d503702a7364b7
Muffinman <albusgithub@mevacy.com>
2024-11-20 08:15:55 -0700

    Lecture Tries start

---
+++ 2.0 CS3 Algorithms Sherine CPP/1. Lecture/18.0 Tries/main.cpp
@@ -0,0 +1,16 @@
+/*
+TRIES DATA STRUCTURE:
+• Very useful for large string data sets.
+• 
+
+*/
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "\nProgram start." << std::endl;
+    
+    std::cout << "\nEnd of program." << std::endl;
+    return 0;
+}
