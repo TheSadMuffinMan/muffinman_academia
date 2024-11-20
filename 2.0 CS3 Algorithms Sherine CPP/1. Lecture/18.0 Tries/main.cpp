38a665b30f2f8f41c5ee0b2107b2ad634b279ac0
Muffinman <albusgithub@mevacy.com>
2024-11-20 08:47:06 -0700

    Trie lecture complete

--- 2.0 CS3 Algorithms Sherine CPP/1. Lecture/18.0 Tries/main.cpp
+++ 2.0 CS3 Algorithms Sherine CPP/1. Lecture/18.0 Tries/main.cpp
@@ -1,7 +1,10 @@
 /*
 TRIES DATA STRUCTURE:
 • Very useful for large string data sets.
-• 
+• Get's its name from the word "retrieval", but is pronounced "try"s.
+• Fundamentally, a Trie stores each char (inside of a string) as a node in a tree.
+    - This allows us to be extremely space and time efficient, as we do not have any wasted space and
+    the search operation traverses the tree very quickly.
 
 */
 
