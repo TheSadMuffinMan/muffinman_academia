f8b8e26553b23e1ecc5cdacc5d24d1fc8afee1a3
Muffinman <albusgithub@mevacy.com>
2025-02-27 13:36:07 -0700

    Stuff for AI Class

---
+++ 3.0 AI Topics/1. Python Q-Learning/Step1.py
@@ -0,0 +1,5 @@
+# X
+import minari
+
+datasets = minari.list_remote_datasets()
+print("Available datasets:", datasets)
---
+++ 3.0 AI Topics/1. Python Q-Learning/Step2.py
@@ -0,0 +1,6 @@
+import minari
+
+# dataset = minari.load_dataset("minari/example-dataset", download=True)
+# dataset = minari.load_dataset("minari/example-dataset-v1", download=True)
+dataset = minari.load_dataset("BabyAI-PutNextLocal-v0", download=True)
+print(dataset)
