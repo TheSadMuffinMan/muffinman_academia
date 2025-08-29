8f849b3d4f2639ddc3f750ddd017a0166d6aaa65
muffins <albusgithub@mevacy.com>
2025-08-29 08:01:07 -0600

    Cleaning up files

--- 9.0 Programming Languages/0. Skeleton Files/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(int argc, char *argv[])
-{
-    std::cout << "\nProgram start." << std::endl;
-    
-    std::cout << "\nEnd of program." << std::endl;
-    return 0;
-}
