42f268a9f12cff98ee2b89b2331df582dc8cdc05
muffins <albusgithub@mevacy.com>
2026-01-06 13:01:19 -0700

    Small amount of code, leaving work

--- 9. LeetCode Problems/2. Add Two Numbers/main.cpp
+++ 9. LeetCode Problems/2. Add Two Numbers/main.cpp
@@ -13,6 +13,10 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {};
 };
 
+struct makeListNode {
+    //
+};
+
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
@@ -21,6 +25,13 @@ public:
 };
 
 int main(int argc, char* argv[]) {
+    int input1, input2;
+    std::cout << "Please input a non-negative number: ";
+    std::cin >> input1;
+
+    std::cout << "Please input another non-negative number: ";
+    std::cin >> input2;
+    
 
     std::cout << "\n\nProgram complete." << std::endl;
     return 0;
