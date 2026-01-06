7559e5ba66a144806efa91b75cef0270e3c6ab2f
muffins <albusgithub@mevacy.com>
2026-01-06 12:38:40 -0700

    Accepted Two Sum problem!

--- 9. LeetCode Problems/1. Two Sum/main.cpp
+++ 9. LeetCode Problems/1. Two Sum/main.cpp
@@ -1,37 +1,55 @@
+// g++ source.cpp -o program
+
 #include <iostream>
 #include <vector>
 
 // For every number inside of the array, add every other number and see if it = target.
 class Solution {
-public:
-    std::vector<int> twoSum(std::vector<int>& nums, int target) {
-        // Loop represents every number inside of nums.
-        for (int i = 0; i < nums.size(); i++) {
-            // Another loop to check nums[i] vs every other number in list.
-            for (int j = 0; j < nums.size(); j++) {
-                // If the address is the same for both nums[i] & nums[j], skip that part of the loop.
-                if (&nums[i] == &nums[j]) continue;
-
-                int result = nums[i] + nums[j];
-
-                if (result == target) {
-                   std::cout << "[" << i << "," << j << "]" << std::endl;
+    public:
+        std::vector<int> twoSum(std::vector<int>& nums, int target) {
+            // i represents every number inside of nums.
+            for (int i = 0; i < nums.size(); i++) {
+                // Skips self comparisons and guarantees unique index pairs.
+                // Starts at (i + 1) to never compare an element w/ itself and checks each unique pair only once.
+                for (int j = (i + 1); j < nums.size(); j++) {
+                    if (nums[i] + nums[j] == target) {
+                        std::cout << "[" << i << "," << j << "]" << std::endl;
+                        return {i, j};
+                    }
                 }
             }
+
+            std::cerr << "\nNo solution found." << std::endl;
+            return {}; // Empty vector = no solution.
         }
-    }
 };
 
 int main(int argc, char* argv[]) {
     std::vector<int> arguments;
-    int num_elements, target;
+    int target, temp_input;
 
     std::cout << "Target: ";
     std::cin >> target;
-    std::cout << "\nNumber elements: ";
-    std::cin >> num_elements;
 
-    for (int i = 0; i < num_elements; i++) {
-        std::cin >> arguments[i];
+    std::cout << "\nInput any number of ints, stop input by inputting any non-int." << std::endl;
+    while (std::cin >> temp_input) {
+        arguments.push_back(temp_input);
     }
+
+    // Clearing the failure state so we can keep using cin. Thank you CS3 lol.
+    std::cin.clear();
+
+    /*
+    // Debugging, been a minute since I've used vectors without AI help :(
+    std::cout << "\n***DEBUG*** You entered:\n";
+    for (int v : arguments) {
+        std::cout << v << " ";
+    }
+    */
+
+    Solution sol;
+    std::vector<int> result = sol.twoSum(arguments, target);
+
+    std::cout << "\n\nProgram complete." << std::endl;
+    return 0;
 }
