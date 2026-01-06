85f547d4da57ea06a9bae2d10b4a7de8490e2aea
muffins <albusgithub@mevacy.com>
2026-01-06 12:56:46 -0700

    Setup for Add Two Numbers

---
+++ 9. LeetCode Problems/2. Add Two Numbers/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    // Constructor:
+    ListNode() : val(0), next(nullptr) {};
+
+    ListNode(int x) : val(x), next(nullptr) {};
+    ListNode(int x, ListNode *next) : val(x), next(next) {};
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+    }
+};
+
+int main(int argc, char* argv[]) {
+
+    std::cout << "\n\nProgram complete." << std::endl;
+    return 0;
+}
